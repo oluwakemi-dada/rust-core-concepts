794522c22f298124f472426d71b5e871fd9725af
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-02-10 13:06:29 +0100

    immutable and mutable variables

--- variables-and-mutability/src/main.rs
+++ variables-and-mutability/src/main.rs
@@ -1,7 +1,15 @@
-fn main() {
-  let apples = 50;
-  let oranges = 14 + 6;
-  let _fruits = apples + oranges;
+// fn main() {
+//   let apples = 50;
+//   let oranges = 14 + 6;
+//   let _fruits = apples + oranges;
 
-  println!("This year, my garden has {0} apples and {1} oranges. I cannot believe i have {1} oranges.", apples, oranges);
+//   println!("This year, my garden has {0} apples and {1} oranges. I cannot believe i have {1} oranges.", apples, oranges);
+// }
+
+fn main() {
+  let mut gym_reps = 10;
+  println!("I plan to do {gym_reps} reps");
+
+  gym_reps = 15;
+    println!("I now plan to do {gym_reps} reps");
 }
