dc715144114e8c6132284e8c48d7d2dc43bd9d2f
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-02-10 12:45:13 +0100

    interpolation with curly braces

--- variables-and-mutability/src/main.rs
+++ variables-and-mutability/src/main.rs
@@ -1,1 +1,7 @@
-fn main() {}
+fn main() {
+  let apples = 50;
+  let oranges = 14 + 6;
+  let fruits = apples + oranges;
+
+  println!("This year, my garden has {} apples and {} oranges making a total of {} fruits", apples, oranges, fruits);
+}
