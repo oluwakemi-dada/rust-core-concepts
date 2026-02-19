3575194bbc6aad03470823b5976891a921d66e5e
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-02-19 20:34:57 +0100

    variable shadowing

--- variables-and-mutability/src/main.rs
+++ variables-and-mutability/src/main.rs
@@ -6,10 +6,21 @@
 //   println!("This year, my garden has {0} apples and {1} oranges. I cannot believe i have {1} oranges.", apples, oranges);
 // }
 
-fn main() {
-  let mut gym_reps = 10;
-  println!("I plan to do {gym_reps} reps");
+// fn main() {
+//   let mut gym_reps = 10;
+//   println!("I plan to do {gym_reps} reps");
 
-  gym_reps = 15;
-    println!("I now plan to do {gym_reps} reps");
+//   gym_reps = 15;
+//     println!("I now plan to do {gym_reps} reps");
+// }
+
+fn main() {
+  let grams_of_protein = "100.345";
+  println!("{grams_of_protein}");
+  let grams_of_protein = 100.345;
+  println!("{grams_of_protein}");
+  let mut grams_of_protein = 100;
+  println!("{grams_of_protein}");
+  grams_of_protein = 105;
+  println!("{grams_of_protein}");
 }
