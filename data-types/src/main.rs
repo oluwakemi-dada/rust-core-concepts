6d633d0b8d6984e2b55192de04384415aa8df78a
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-02-20 23:11:01 +0100

    underscore as visual separator

--- data-types/src/main.rs
+++ data-types/src/main.rs
@@ -1,9 +1,15 @@
+// ---- Integers ----
+// fn main() {
+//   let sixteen_bit_signed: i16 = -32500;
+//   let sixteen_bit_unsigned: u16 = 64000;
+
+//   let thirty_two_bit_signed: i32 = -2147483648;
+//   let thirty_two_bit_unsigned: u32 = 4294967295;
+
+//   let some_value = 20u16;
+// }
+
+// ---- Underscore "_" as visual separator
 fn main() {
-  let sixteen_bit_signed: i16 = -32500;
-  let sixteen_bit_unsigned: u16 = 64000;
-
-  let thirty_two_bit_signed: i32 = -2147483648;
-  let thirty_two_bit_unsigned: u32 = 4294967295;
-
-  let some_value = 20u16;
+  let sixteen_bit_signed: i32 = 320_500;
 }
