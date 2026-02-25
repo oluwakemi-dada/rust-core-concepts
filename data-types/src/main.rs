ddfdc7a07fa1f299bcffb3f92d6554bcce18cf1c
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-02-25 13:36:30 +0100

    cast types with the as keyword

--- data-types/src/main.rs
+++ data-types/src/main.rs
@@ -52,7 +52,19 @@
 // }
 
 //  ---- Formatting Floats with Format Specifier ----
+// fn main(){
+//   let pi: f64 = 3.141593458972679344785973674;
+//   println!("The current value of pi is {0} and the formatted value of pi is {0:.3}", pi);
+// }
+
+//  ---- Casting Types with the as Keyword ----
 fn main(){
-  let pi: f64 = 3.141593458972679344785973674;
-  println!("The current value of pi is {0} and the formatted value of pi is {0:.3}", pi);
+  let miles_away = 50;
+  let miles_away_i8 = miles_away as i8;
+
+  let miles_away = 100.329032;
+  let miles_away_f32 = miles_away as f32;
+  let miles_away_int = miles_away as i32;
+
+  println!("{miles_away_int}")
 }
