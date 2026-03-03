a26eefb019917b1c362e637acc00a0221ea9c6f2
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-03 13:38:57 +0100

    intro to functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,1 +1,20 @@
-fn main() {}
+// ---- Intro to Functions ----
+fn main() {
+    open_store();
+    bake_pizza();
+    swim_in_profit();
+    swim_in_profit();
+    swim_in_profit();
+}
+
+fn open_store() {
+    println!("Opening my pizza store")
+}
+
+fn bake_pizza() {
+    println!("Baking a pizza")
+}
+
+fn swim_in_profit() {
+    println!("So much $$$, so little time");
+}
