2d840dde63a00edffd7c97e6cd5e105601400647
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-03 19:00:54 +0100

    the if statement

--- control-flow/src/main.rs
+++ control-flow/src/main.rs
@@ -1,1 +1,12 @@
-fn main() {}
+// ---- The if Statement ----
+fn main() {
+    let some_condition_that_we_cannot_predict_in_advance = true;
+
+    if some_condition_that_we_cannot_predict_in_advance {
+        println!("This line will be output");
+    }
+
+    if false {
+        println!("This line will NOT be output");
+    }
+}
