4de74c232e0e632b7948e7462cfaa26b029db6ef
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-03 19:09:11 +0100

    the else if statement

--- control-flow/src/main.rs
+++ control-flow/src/main.rs
@@ -1,12 +1,27 @@
 // ---- The if Statement ----
+// fn main() {
+//     let some_condition_that_we_cannot_predict_in_advance = true;
+
+//     if some_condition_that_we_cannot_predict_in_advance {
+//         println!("This line will be output");
+//     }
+
+//     if false {
+//         println!("This line will NOT be output");
+//     }
+// }
+
+// ---- The else if Statement ----
 fn main() {
-    let some_condition_that_we_cannot_predict_in_advance = true;
+    let season = "summer";
 
-    if some_condition_that_we_cannot_predict_in_advance {
-        println!("This line will be output");
-    }
-
-    if false {
-        println!("This line will NOT be output");
+    if season == "summer" {
+        println!("School's out!");
+    } else if season == "winter" {
+        println!("Brr, so cold!");
+    } else if season == "fall" {
+        println!("Leaves falling!")
+    } else if season == "spring" {
+        println!("Lots of rain!")
     }
 }
