d4fbc0ae0172f44e503c57633c0a2e9884421a4a
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-05 21:04:29 +0100

    the continue keyword

--- control-flow/src/main.rs
+++ control-flow/src/main.rs
@@ -97,3 +97,24 @@
 //         seconds -= 1;
 //     }
 // }
+
+// ---- The continue Keyword ----
+fn main() {
+    let mut seconds = 21;
+
+    loop {
+        if seconds <= 0 {
+            println!("Blastoff!");
+            break;
+        }
+
+        if seconds % 2 == 0 {
+            println!("{seconds} seconds (even number), skipping 3 seconds...");
+            seconds -= 3;
+            continue;
+        }
+
+        println!("{seconds} seconds to blastoff...");
+        seconds -= 1;
+    }
+}
