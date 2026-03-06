cd83fb9c7933e7e523f3da84b15137f17b79320b
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-06 21:22:27 +0100

    coding challenge

--- control-flow/src/main.rs
+++ control-flow/src/main.rs
@@ -151,18 +151,58 @@
 // }
 
 // ---- Debugging ----
-fn countdown(seconds: i32) {
-    if seconds == 0 {
-        println!("Blastoff!");
-    } else {
-        println!("{seconds} seconds to blastoff...");
-        countdown(seconds - 1);
+// fn countdown(seconds: i32) {
+//     if seconds == 0 {
+//         println!("Blastoff!");
+//     } else {
+//         println!("{seconds} seconds to blastoff...");
+//         countdown(seconds - 1);
+//     }
+// }
+// fn main() {
+//     countdown(6);
+
+//     countdown(5);
+
+//     countdown(4);
+// }
+
+// ---- Coding Challenge ----
+fn color_to_number(color: &str) -> i32 {
+    match color {
+        "red" => 1,
+        "green" => 2,
+        "blue" => 3,
+        _ => 0,
     }
 }
+
+fn factorial_iterative(number: i32) -> i32 {
+    let mut product = 1;
+    let mut count = number;
+
+    while count > 0 {
+        product *= count;
+        count -= 1;
+    }
+
+    product
+}
+
+fn factorial_recursive(number: i32) -> i32 {
+    if  number == 1 {
+        return 1;
+    } else {
+        return number * factorial_recursive(number - 1);
+    }
+}
+
 fn main() {
-    countdown(6);
+    println!("{}", color_to_number("red"));
+    println!("{}", color_to_number("green"));
+    println!("{}", color_to_number("blue"));
+    println!("{}", color_to_number("purple"));
 
-    countdown(5);
-
-    countdown(4);
+    println!("{}", factorial_recursive(5));
+    println!("{}", factorial_iterative(5));
 }
