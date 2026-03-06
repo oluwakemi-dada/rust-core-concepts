1a56e2d6cf1b4aaa76dc61ebabe25608235716e7
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-06 21:59:47 +0100

    the copy trait

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,10 +1,18 @@
 // ---- Scope and Ownership ----
+// fn main() {
+//     let age = 33;
+//     let is_handsome = true;
+
+//     println!("{age}");
+//     println!("{is_handsome}");
+
+//     // age variable exists here
+// } // is_handsome goes out of scope, then age variable goes out of scope here
+
+// ---- The Copy Trait ----
 fn main() {
-    let age = 33;
-    let is_handsome = true;
+    let time = 2026;
+    let years = time;
 
-    println!("{age}");
-    println!("{is_handsome}");
-
-    // age variable exists here
-} // is_handsome goes out of scope, then age variable goes out of scope here
+    println!("The time is {time}. It is the year {years}.");
+}
