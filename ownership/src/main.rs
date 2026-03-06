385ec7600b9a454e654dc4a050675a183674500c
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-06 21:56:27 +0100

    scope and ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,1 +1,10 @@
-fn main() {}
+// ---- Scope and Ownership ----
+fn main() {
+    let age = 33;
+    let is_handsome = true;
+
+    println!("{age}");
+    println!("{is_handsome}");
+
+    // age variable exists here
+} // is_handsome goes out of scope, then age variable goes out of scope here
