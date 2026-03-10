1e46487e10d2c6a85c5f3b516c7611a03dadec33
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-10 10:03:50 +0100

    the dereference operator

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -65,10 +65,25 @@
 // }
 
 // ---- References and Borrowing ----
+// fn main() {
+//     let my_stack_value = 2;
+//     let my_integer_reference = &my_stack_value;
+
+//     let my_heap_value = String::from("Toyota");
+//     let my_heap_reference = &my_heap_value;
+// }
+
+// ---- Dereference Operator ----
 fn main() {
     let my_stack_value = 2;
     let my_integer_reference = &my_stack_value;
 
+    println!("{}", my_integer_reference);
+    println!("{}", *my_integer_reference);
+
     let my_heap_value = String::from("Toyota");
     let my_heap_reference = &my_heap_value;
+
+    println!("{}", my_heap_reference);
+    println!("{}", *my_heap_reference);
 }
