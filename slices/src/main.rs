479fa44311a1aa3fb29af504b1a918553dd093eb
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-12 16:22:57 +0100

    create a string slice from a string

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,1 +1,9 @@
-fn main() {}
+// ---- Create a String Slice from a String ----
+fn main() {
+    let action_hero = String::from("Arnold Schwarzenegger");
+    let first_name = &action_hero[0..6];
+    println!("{first_name}");
+
+    let last_name = &action_hero[7..21];
+    println!("{last_name}")
+}
