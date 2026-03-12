12925d91bb66c18f1fb70ef9ab7af623097de8c1
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-12 16:36:35 +0100

    string slices and string literals

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,9 +1,19 @@
 // ---- Create a String Slice from a String ----
-fn main() {
-    let action_hero = String::from("Arnold Schwarzenegger");
-    let first_name = &action_hero[0..6];
-    println!("{first_name}");
+// fn main() {
+//     let action_hero = String::from("Arnold Schwarzenegger");
+//     let first_name = &action_hero[0..6];
+//     println!("{first_name}");
 
-    let last_name = &action_hero[7..21];
-    println!("{last_name}")
+//     let last_name = &action_hero[7..21];
+//     println!("{last_name}")
+// }
+
+// ---- String Slices and String Literals ----
+fn main() {
+    let first_name = {
+        let action_hero = "Arnold Schwarzenegger";
+        &action_hero[0..6]
+    };
+
+    println!("{first_name}");
 }
