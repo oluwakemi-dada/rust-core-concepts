9937e000340d4e188dfea6ca5bf096c92a617f09
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-13 22:03:12 +0100

    overwrite struct fields

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,4 +1,28 @@
 // ---- Define a Struct | Struct instance | Access Struct Fields ----
+// fn main() {
+//     struct Coffee {
+//         name: String,
+//         price: f64,
+//         is_hot: bool,
+//     }
+
+//     let mocha = Coffee {
+//         name: String::from("Mocha"),
+//         price: 4.99,
+//         is_hot: false,
+//     };
+
+//     println!(
+//         "My {} this morning cost {}. It is {} that it was hot",
+//         mocha.name, mocha.price, mocha.is_hot
+//     );
+
+//     let favorite_coffee = mocha.name;
+//     println!("{favorite_coffee}");
+//     // println!("{}", mocha.name)
+// }
+
+// ---- Overwrite Struct Fields ----
 fn main() {
     struct Coffee {
         name: String,
@@ -6,18 +30,18 @@ fn main() {
         is_hot: bool,
     }
 
-    let mocha = Coffee {
+    let mut beverage = Coffee {
         name: String::from("Mocha"),
         price: 4.99,
         is_hot: false,
     };
 
+    beverage.name = String::from("Caramel Macchiato");
+    beverage.price = 6.99;
+    beverage.is_hot = true;
+
     println!(
         "My {} this morning cost {}. It is {} that it was hot",
-        mocha.name, mocha.price, mocha.is_hot
+        beverage.name, beverage.price, beverage.is_hot
     );
-
-    let favorite_coffee = mocha.name;
-    println!("{favorite_coffee}");
-    // println!("{}", mocha.name)
 }
