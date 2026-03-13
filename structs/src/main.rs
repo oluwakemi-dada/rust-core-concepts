f94ed44932622f3945e36c87cfbb92d3f322ba37
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-13 21:50:03 +0100

    access struct fields

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,4 +1,4 @@
-// ---- Define a Struct | Struct instance ----
+// ---- Define a Struct | Struct instance | Access Struct Fields ----
 fn main() {
     struct Coffee {
         name: String,
@@ -9,6 +9,15 @@ fn main() {
     let mocha = Coffee {
         name: String::from("Mocha"),
         price: 4.99,
-        is_hot: true,
+        is_hot: false,
     };
+
+    println!(
+        "My {} this morning cost {}. It is {} that it was hot",
+        mocha.name, mocha.price, mocha.is_hot
+    );
+
+    let favorite_coffee = mocha.name;
+    println!("{favorite_coffee}");
+    // println!("{}", mocha.name)
 }
