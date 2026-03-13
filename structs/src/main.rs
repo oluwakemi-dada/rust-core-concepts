c989742c1a91e04daf9c1af813ccfd4b8f480cc6
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-13 21:30:36 +0100

    struct instance

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,8 +1,14 @@
-// ---- Define a Struct ----
+// ---- Define a Struct | Struct instance ----
 fn main() {
     struct Coffee {
-        price: f64,
         name: String,
+        price: f64,
         is_hot: bool,
     }
+
+    let mocha = Coffee {
+        name: String::from("Mocha"),
+        price: 4.99,
+        is_hot: true,
+    };
 }
