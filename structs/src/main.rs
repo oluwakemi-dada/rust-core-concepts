9b5999e273c77092eb1aaa8d2005496100a9bd4f
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-13 22:58:27 +0100

    struct update syntax

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -47,6 +47,41 @@
 // }
 
 // ---- Create Structs in a Function | Struct Field Initialization Shorthand Syntax ----
+// struct Coffee {
+//     name: String,
+//     price: f64,
+//     is_hot: bool,
+// }
+
+// fn main() {
+//     let name = String::from("Latte");
+//     let coffee = make_coffee(name, 4.99, true);
+//     println!(
+//         "My {} this morning cost {}. It is {} that it was hot.",
+//         coffee.name, coffee.price, coffee.is_hot
+//     );
+//     // println!("{name}");
+
+//     let name = String::from("Latte");
+//     let price = 3.99;
+//     let is_hot = false;
+
+//     let latte = Coffee {
+//         name,
+//         price,
+//         is_hot,
+//     };
+// }
+
+// fn make_coffee(name: String, price: f64, is_hot: bool) -> Coffee {
+//     Coffee {
+//         name,
+//         price,
+//         is_hot,
+//     }
+// }
+
+// ---- Struct Update Syntax ----
 struct Coffee {
     name: String,
     price: f64,
@@ -54,23 +89,15 @@ struct Coffee {
 }
 
 fn main() {
-    let name = String::from("Latte");
-    let coffee = make_coffee(name, 4.99, true);
-    println!(
-        "My {} this morning cost {}. It is {} that it was hot.",
-        coffee.name, coffee.price, coffee.is_hot
-    );
-    // println!("{name}");
+    let mocha = make_coffee(String::from("Mocha"), 4.99, true);
 
-    let name = String::from("Latte");
-    let price = 3.99;
-    let is_hot = false;
-
-    let latte = Coffee {
-        name,
-        price,
-        is_hot,
+    let caramel_macchiato = Coffee {
+        name: mocha.name.clone(),
+        ..mocha
     };
+
+    println!("{}", caramel_macchiato.name);
+    println!("{}", mocha.name);
 }
 
 fn make_coffee(name: String, price: f64, is_hot: bool) -> Coffee {
