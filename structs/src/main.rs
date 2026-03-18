0663ca6c93f05833da151a92ce8f409364f6a32e
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-18 15:56:25 +0100

    multiple impl blocks

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -354,6 +354,48 @@
 // }
 
 // ---- Associated Functions ----
+// #[derive(Debug)]
+// struct TaylorSwiftSong {
+//     title: String,
+//     release_year: u32,
+//     duration_secs: u32,
+// }
+
+// impl TaylorSwiftSong {
+//     fn new(title: String, release_year: u32, duration_secs: u32) -> Self {
+//         Self {
+//             title,
+//             release_year,
+//             duration_secs,
+//         }
+//     }
+
+//     fn display_song_info(&self) {
+//         println!("Title: {}", self.title);
+//         println!("Years Since Release: {}", self.years_since_release());
+//         println!("Duration: {}", self.duration_secs);
+//     }
+
+//     fn double_length(&mut self) {
+//         self.duration_secs = self.duration_secs * 2;
+//     }
+
+//     fn is_longer_than(&self, other: &Self) -> bool {
+//         self.duration_secs > other.duration_secs
+//     }
+
+//     fn years_since_release(&self) -> u32 {
+//         2026 - self.release_year
+//     }
+// }
+
+// fn main() {
+//     let blank_space = TaylorSwiftSong::new(String::from("Blank Space"), 2014, 231);
+
+//     blank_space.display_song_info();
+// }
+
+// ---- Multiple impl Blocks ----
 #[derive(Debug)]
 struct TaylorSwiftSong {
     title: String,
@@ -369,7 +411,9 @@ impl TaylorSwiftSong {
             duration_secs,
         }
     }
+}
 
+impl TaylorSwiftSong {
     fn display_song_info(&self) {
         println!("Title: {}", self.title);
         println!("Years Since Release: {}", self.years_since_release());
