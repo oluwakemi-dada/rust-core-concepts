9d569c4d711651224134bfa8fa5ecdddde51c4f6
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-18 16:55:39 +0100

    builder pattern

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -396,45 +396,88 @@
 // }
 
 // ---- Multiple impl Blocks ----
+// #[derive(Debug)]
+// struct TaylorSwiftSong {
+//     title: String,
+//     release_year: u32,
+//     duration_secs: u32,
+// }
+
+// impl TaylorSwiftSong {
+//     fn new(title: String, release_year: u32, duration_secs: u32) -> Self {
+//         Self {
+//             title,
+//             release_year,
+//             duration_secs,
+//         }
+//     }
+// }
+
+// impl TaylorSwiftSong {
+//     fn display_song_info(&self) {
+//         println!("Title: {}", self.title);
+//         println!("Years Since Release: {}", self.years_since_release());
+//         println!("Duration: {}", self.duration_secs);
+//     }
+
+//     fn double_length(&mut self) {
+//         self.duration_secs = self.duration_secs * 2;
+//     }
+
+//     fn is_longer_than(&self, other: &Self) -> bool {
+//         self.duration_secs > other.duration_secs
+//     }
+
+//     fn years_since_release(&self) -> u32 {
+//         2026 - self.release_year
+//     }
+// }
+
+// fn main() {
+//     let blank_space = TaylorSwiftSong::new(String::from("Blank Space"), 2014, 231);
+
+//     blank_space.display_song_info();
+// }
+
+// ---- Builder Pattern ----
 #[derive(Debug)]
-struct TaylorSwiftSong {
-    title: String,
-    release_year: u32,
-    duration_secs: u32,
+struct Computer {
+    cpu: String,
+    memory: u32,
+    hard_drive_capacity: u32,
 }
 
-impl TaylorSwiftSong {
-    fn new(title: String, release_year: u32, duration_secs: u32) -> Self {
+impl Computer {
+    fn new(cpu: String, memory: u32, hard_drive_capacity: u32) -> Self {
         Self {
-            title,
-            release_year,
-            duration_secs,
+            cpu,
+            memory,
+            hard_drive_capacity,
         }
     }
-}
 
-impl TaylorSwiftSong {
-    fn display_song_info(&self) {
-        println!("Title: {}", self.title);
-        println!("Years Since Release: {}", self.years_since_release());
-        println!("Duration: {}", self.duration_secs);
+    fn upgrade_cpu(&mut self, new_cpu: String) -> &mut Self {
+        self.cpu = new_cpu;
+        self
     }
 
-    fn double_length(&mut self) {
-        self.duration_secs = self.duration_secs * 2;
+    fn upgrade_memory(&mut self, new_memory: u32) -> &mut Self {
+        self.memory = new_memory;
+        self
     }
 
-    fn is_longer_than(&self, other: &Self) -> bool {
-        self.duration_secs > other.duration_secs
-    }
-
-    fn years_since_release(&self) -> u32 {
-        2026 - self.release_year
+    fn upgrade_hard_drive_capacity(&mut self, new_capacity: u32) -> &mut Self {
+        self.hard_drive_capacity = new_capacity;
+        self
     }
 }
-
 fn main() {
-    let blank_space = TaylorSwiftSong::new(String::from("Blank Space"), 2014, 231);
+    let mut computer = Computer::new(String::from("M3 Max"), 64, 2);
 
-    blank_space.display_song_info();
+    computer
+        .upgrade_cpu(String::from("M4 Max"))
+        .upgrade_memory(128)
+        .upgrade_hard_drive_capacity(4);
+
+    println!("Stats: {:#?}", computer);
 }
