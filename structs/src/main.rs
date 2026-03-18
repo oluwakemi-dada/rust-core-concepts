7fcd78302cff5eb948b86a70fbe7fb383d468a7a
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-18 15:51:09 +0100

    associated functions

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -316,6 +316,44 @@
 // }
 
 // ---- Calling Methods from Other Methods ----
+// #[derive(Debug)]
+// struct TaylorSwiftSong {
+//     title: String,
+//     release_year: u32,
+//     duration_secs: u32,
+// }
+
+// impl TaylorSwiftSong {
+//     fn display_song_info(&self) {
+//         println!("Title: {}", self.title);
+//         println!("Years Since Release: {}", self.years_since_release());
+//         println!("Duration: {}", self.duration_secs);
+//     }
+
+//     fn double_length(&mut self) {
+//         self.duration_secs = self.duration_secs * 2;
+//     }
+
+//     fn is_longer_than(&self, other: &Self) -> bool {
+//         self.duration_secs > other.duration_secs
+//     }
+
+//     fn years_since_release(&self) -> u32 {
+//         2026 - self.release_year
+//     }
+// }
+
+// fn main() {
+//     let blank_space = TaylorSwiftSong {
+//         title: String::from("Blank Space"),
+//         release_year: 2014,
+//         duration_secs: 231,
+//     };
+
+//     blank_space.display_song_info();
+// }
+
+// ---- Associated Functions ----
 #[derive(Debug)]
 struct TaylorSwiftSong {
     title: String,
@@ -324,6 +362,14 @@ struct TaylorSwiftSong {
 }
 
 impl TaylorSwiftSong {
+    fn new(title: String, release_year: u32, duration_secs: u32) -> Self {
+        Self {
+            title,
+            release_year,
+            duration_secs,
+        }
+    }
+
     fn display_song_info(&self) {
         println!("Title: {}", self.title);
         println!("Years Since Release: {}", self.years_since_release());
@@ -344,11 +390,7 @@ impl TaylorSwiftSong {
 }
 
 fn main() {
-    let blank_space = TaylorSwiftSong {
-        title: String::from("Blank Space"),
-        release_year: 2014,
-        duration_secs: 231,
-    };
+    let blank_space = TaylorSwiftSong::new(String::from("Blank Space"), 2014, 231);
 
     blank_space.display_song_info();
 }
