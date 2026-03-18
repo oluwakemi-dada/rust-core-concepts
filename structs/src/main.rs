2c0c80bc442d673e70a74fc21bb74e704d4952a8
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-18 15:00:37 +0100

    methods with multiple parameters

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -228,6 +228,44 @@
 // }
 
 // ---- self Parameter as Immutable and Mutable References to Struct Instance ----
+// #[derive(Debug)]
+// struct TaylorSwiftSong {
+//     title: String,
+//     release_year: u32,
+//     duration_secs: u32,
+// }
+
+// impl TaylorSwiftSong {
+//     // Immutable struct value (self parameter takes ownership)
+//     // -> Immutable reference to the struct instance (no ownership moved)
+//     fn display_song_info(&self) {
+//         // self: &Self
+//         println!("Title: {}", self.title);
+//         println!("Release Year: {}", self.release_year);
+//         println!("Duration: {}", self.duration_secs);
+//     }
+
+//     // Mutable struct value (self parameter takes ownership, has permission to mutate)
+//     // -> Mutable reference to the struct instance (no ownership moved, has permission to mutate)
+//     fn double_length(&mut self) {
+//         // self: &mut Self
+//         self.duration_secs = self.duration_secs * 2;
+//     }
+// }
+
+// fn main() {
+//     let mut song = TaylorSwiftSong {
+//         title: String::from("Blank Space"),
+//         release_year: 2014,
+//         duration_secs: 231,
+//     };
+
+//     song.display_song_info(); // &song
+//     song.double_length();
+//     song.display_song_info();
+// }
+
+// ---- Methods with Multiple Parameters ----
 #[derive(Debug)]
 struct TaylorSwiftSong {
     title: String,
@@ -236,31 +274,43 @@ struct TaylorSwiftSong {
 }
 
 impl TaylorSwiftSong {
-    // Immutable struct value (self parameter takes ownership)
-    // -> Immutable reference to the struct instance (no ownership moved)
     fn display_song_info(&self) {
-        // self: &Self
         println!("Title: {}", self.title);
         println!("Release Year: {}", self.release_year);
         println!("Duration: {}", self.duration_secs);
     }
 
-    // Mutable struct value (self parameter takes ownership, has permission to mutate)
-    // -> Mutable reference to the struct instance (no ownership moved, has permission to mutate)
     fn double_length(&mut self) {
-        // self: &mut Self
         self.duration_secs = self.duration_secs * 2;
+    }
+
+    fn is_longer_than(&self, other: &Self) -> bool {
+        self.duration_secs > other.duration_secs
     }
 }
 
 fn main() {
-    let mut song = TaylorSwiftSong {
+    let blank_space = TaylorSwiftSong {
         title: String::from("Blank Space"),
         release_year: 2014,
         duration_secs: 231,
     };
 
-    song.display_song_info(); // &song
-    song.double_length();
-    song.display_song_info();
+    let all_too_well = TaylorSwiftSong {
+        title: String::from("All Too Well"),
+        release_year: 2012,
+        duration_secs: 327,
+    };
+
+    if blank_space.is_longer_than(&all_too_well) {
+        println!(
+            "{} is longer than {}",
+            blank_space.title, all_too_well.title
+        )
+    } else {
+        println!(
+            "{} is shorter than or equal to {}",
+            blank_space.title, all_too_well.title,
+        )
+    }
 }
