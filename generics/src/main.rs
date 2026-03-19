59ecdc43aed24ca28e809c0264396197cf4ae36a
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-19 20:55:09 +0100

    intro to generics

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,1 +1,17 @@
-fn main() {}
+// ---- Intro to Generics ----
+#[derive(Debug)]
+struct DeliSandwich {}
+fn main() {
+    println!("{}", identity::<i32>(5));
+    println!("{}", identity::<i8>(5));
+    println!("{}", identity::<u32>(5));
+    println!("{}", identity::<f64>(13.5));
+    println!("{}", identity::<&str>("Hello"));
+    println!("{}", identity::<String>(String::from("Hi there!")));
+    println!("{}", identity::<bool>(true));
+    println!("{:?}", identity::<DeliSandwich>(DeliSandwich {}));
+}
+
+fn identity<T>(value: T) -> T {
+    value
+}
