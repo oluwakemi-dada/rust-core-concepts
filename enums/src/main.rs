4c3450b0f86270a9793b839c81df436d29a97178
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-19 12:56:12 +0100

    enums with associated values

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,1 +1,40 @@
-fn main() {}
+// ---- Intro to Enums ----
+// #[derive(Debug)]
+// enum CardSuit {
+//     Hearts,
+//     Diamond,
+//     Spades,
+//     Clubs,
+// }
+
+// struct Card {
+//     rank: String,
+//     suit: CardSuit,
+// }
+
+// fn main() {
+//     let first_card = CardSuit::Hearts;
+//     let mut second_card = CardSuit::Spades;
+//     second_card = CardSuit::Clubs;
+
+//     println!("{:?}", second_card);
+
+//     let card_suits = [CardSuit::Hearts, CardSuit::Clubs];
+//     let card_suits = (CardSuit::Hearts, CardSuit::Spades);
+// }
+
+// ---- Enum with Associated Values ----
+#[derive(Debug)]
+enum PaymentMethodType {
+    CreditCard(String),
+    DebitCard(String),
+    PayPal(String, String),
+}
+fn main() {
+    let mut my_payment_method = PaymentMethodType::CreditCard(String::from("0012-3456"));
+
+    my_payment_method =
+        PaymentMethodType::PayPal(String::from("john@email.com"), String::from("password"));
+
+    println!("{:?}", my_payment_method)
+}
