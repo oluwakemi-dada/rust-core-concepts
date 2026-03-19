0a373dd207ce79af4c76ddf5bfe44ab80b8976a0
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-19 21:06:09 +0100

    multiple generics

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,17 +1,29 @@
 // ---- Intro to Generics ----
-#[derive(Debug)]
-struct DeliSandwich {}
-fn main() {
-    println!("{}", identity::<i32>(5));
-    println!("{}", identity::<i8>(5));
-    println!("{}", identity::<u32>(5));
-    println!("{}", identity::<f64>(13.5));
-    println!("{}", identity::<&str>("Hello"));
-    println!("{}", identity::<String>(String::from("Hi there!")));
-    println!("{}", identity::<bool>(true));
-    println!("{:?}", identity::<DeliSandwich>(DeliSandwich {}));
+// #[derive(Debug)]
+// struct DeliSandwich {}
+// fn main() {
+//     println!("{}", identity::<i32>(5));
+//     println!("{}", identity::<i8>(5));
+//     println!("{}", identity::<u32>(5));
+//     println!("{}", identity::<f64>(13.5));
+//     println!("{}", identity::<&str>("Hello"));
+//     println!("{}", identity::<String>(String::from("Hi there!")));
+//     println!("{}", identity::<bool>(true));
+//     println!("{:?}", identity::<DeliSandwich>(DeliSandwich {}));
+// }
+
+// fn identity<T>(value: T) -> T {
+//     value
+// }
+
+// ---- Multiple Generics ----
+fn make_tuple<T, U>(first: T, second: U) -> (T, U) {
+    (first, second)
 }
 
-fn identity<T>(value: T) -> T {
-    value
+fn main() {
+    make_tuple("Hello", 5);
+    make_tuple(13, 5);
+    make_tuple(true, 3.5);
+    make_tuple(true, false);
 }
