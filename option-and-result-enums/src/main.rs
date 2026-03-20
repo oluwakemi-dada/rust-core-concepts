160a2b9983f939eee924fe8947caf47ddbf145f7
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-20 22:19:07 +0100

    result methods

--- option-and-result-enums/src/main.rs
+++ option-and-result-enums/src/main.rs
@@ -161,7 +161,25 @@
 //     println!("{:?}", text_as_number);
 // }
 
-// ---- Returning a Result Enum from a Function ----
+// ---- Returning a Result Enum from a Function | Result Methods----
+// fn divide(numerator: f64, denominator: f64) -> Result<f64, String> {
+//     if denominator == 0.0 {
+//         Err("Cannot divide by zero".to_string())
+//     } else {
+//         Ok(numerator / denominator)
+//     }
+// }
+
+// fn main() {
+//     let result = divide(10.0, 0.0);
+
+//     match result {
+//         Ok(calculation) => println!("Result: {}", calculation),
+//         Err(message) => println!("Error: {}", message),
+//     }
+// }
+
+// ---- Result Methods----
 fn divide(numerator: f64, denominator: f64) -> Result<f64, String> {
     if denominator == 0.0 {
         Err("Cannot divide by zero".to_string())
@@ -171,10 +189,8 @@ fn divide(numerator: f64, denominator: f64) -> Result<f64, String> {
 }
 
 fn main() {
-    let result = divide(10.0, 0.0);
+    let result = divide(10.0, 2.0);
 
-    match result {
-        Ok(calculation) => println!("Result: {}", calculation),
-        Err(message) => println!("Error: {}", message),
-    }
+  println!("{}", result.is_ok());
+  println!("{}", result.is_err());
 }
