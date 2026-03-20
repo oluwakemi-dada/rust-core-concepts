a0aafabaacd6b92e977cf651c7284a88aec008f1
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-20 15:46:57 +0100

    the option enum example

--- option-and-result-enums/src/main.rs
+++ option-and-result-enums/src/main.rs
@@ -1,11 +1,26 @@
 // ---- The Option Enum ----
+// fn main() {
+//     let a = Option::Some(5);
+//     let b = Option::Some("hello");
+//     let c = Option::Some(true);
+
+//     let a: Option<i8> = Option::Some(5);
+//     let b = Option::<i16>::Some(5);
+
+//     let d: Option<&str> = Option::None;
+// }
+
+// ---- The Option Enum Example ----
 fn main() {
-    let a = Option::Some(5);
-    let b = Option::Some("hello");
-    let c = Option::Some(true);
+    let musical_instruments = [
+        String::from("Guitar"),
+        String::from("Drums"),
+        String::from("Bass"),
+    ];
 
-    let a: Option<i8> = Option::Some(5);
-    let b = Option::<i16>::Some(5);
-
-    let d: Option<&str> = Option::None;
+    let bass = musical_instruments.get(2);
+    println!("{:?}", bass);
+    
+    let invalid_instrument = musical_instruments.get(10);
+    println!("{:?}", invalid_instrument);
 }
