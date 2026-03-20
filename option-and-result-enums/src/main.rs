9e85b187ce4697f6c5559a523f738c107d15c797
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-20 12:34:35 +0100

    the option enum

--- option-and-result-enums/src/main.rs
+++ option-and-result-enums/src/main.rs
@@ -1,1 +1,11 @@
-fn main() {}
+// ---- The Option Enum ----
+fn main() {
+    let a = Option::Some(5);
+    let b = Option::Some("hello");
+    let c = Option::Some(true);
+
+    let a: Option<i8> = Option::Some(5);
+    let b = Option::<i16>::Some(5);
+
+    let d: Option<&str> = Option::None;
+}
