97fdb71c1220121f215159cbf8ff169e88e7193e
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-20 18:18:46 +0100

    top-level option variants

--- option-and-result-enums/src/main.rs
+++ option-and-result-enums/src/main.rs
@@ -57,13 +57,35 @@
 // }
 
 // ---- Returning an Option Enum from a Function ----
+// fn is_item_in_stock(item_is_in_system: bool, item_is_in_stock: bool) -> Option<bool> {
+//     if item_is_in_system && item_is_in_stock {
+//         Option::Some(true)
+//     } else if item_is_in_system {
+//         Option::Some(false)
+//     } else {
+//         Option::None
+//     }
+// }
+
+// fn main() {
+//     let availability = is_item_in_stock(true, false);
+
+//     match availability {
+//         // Option::Some(value) => println!("Item is available: {value}"),
+//         Option::Some(true) => println!("Yes, the item is available"),
+//         Option::Some(false) => println!("No, the item is not in stock"),
+//         Option::None => println!("Your item doesn't exist in our system"),
+//     }
+// }
+
+// ---- Top-Level Option Variants ----
 fn is_item_in_stock(item_is_in_system: bool, item_is_in_stock: bool) -> Option<bool> {
     if item_is_in_system && item_is_in_stock {
-        Option::Some(true)
+        Some(true)
     } else if item_is_in_system {
-        Option::Some(false)
+        Some(false)
     } else {
-        Option::None
+        None
     }
 }
 
@@ -72,8 +94,8 @@ fn main() {
 
     match availability {
         // Option::Some(value) => println!("Item is available: {value}"),
-        Option::Some(true) => println!("Yes, the item is available"),
-        Option::Some(false) => println!("No, the item is not in stock"),
-        Option::None => println!("Your item doesn't exist in our system"),
+        Some(true) => println!("Yes, the item is available"),
+        Some(false) => println!("No, the item is not in stock"),
+        None => println!("Your item doesn't exist in our system"),
     }
 }
