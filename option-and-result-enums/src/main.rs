78c8523b27792eb113bd0f66c4e68038021104db
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-20 17:54:07 +0100

    the match keyword with option enum

--- option-and-result-enums/src/main.rs
+++ option-and-result-enums/src/main.rs
@@ -11,6 +11,28 @@
 // }
 
 // ---- The Option Enum Example | The unwrap and expect Methods ----
+// fn main() {
+//     let musical_instruments = [
+//         String::from("Guitar"),
+//         String::from("Drums"),
+//         String::from("Bass"),
+//     ];
+
+//     let bass = musical_instruments.get(2);
+//     println!("{:?}", bass);
+//     let valid_instrument = bass.expect("Unable to retrieve element");
+//     println!("{valid_instrument}");
+
+//     let invalid_instrument = musical_instruments.get(10);
+//     println!("{:?}", invalid_instrument);
+//     // println!("{}", invalid_instrument.unwrap());
+//     println!(
+//         "{}",
+//         invalid_instrument.expect("Unable to retrieve element")
+//     );
+// }
+
+// ---- The match Keyword with Option Enum ----
 fn main() {
     let musical_instruments = [
         String::from("Guitar"),
@@ -19,15 +41,17 @@ fn main() {
     ];
 
     let bass = musical_instruments.get(2);
-    println!("{:?}", bass);
-    let valid_instrument = bass.expect("Unable to retrieve element");
-    println!("{valid_instrument}");
+
+    play(bass); // copy trait implemented here
 
     let invalid_instrument = musical_instruments.get(10);
-    println!("{:?}", invalid_instrument);
-    // println!("{}", invalid_instrument.unwrap());
-    println!(
-        "{}",
-        invalid_instrument.expect("Unable to retrieve element")
-    );
+
+    play(invalid_instrument); // copy trait implemented here
+}
+
+fn play(instrument_option: Option<&String>) {
+    match instrument_option {
+        Option::Some(instrument_option) => println!("Playing the {instrument_option}"),
+        Option::None => println!("Singing with my voice"),
+    }
 }
