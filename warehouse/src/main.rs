34988f2731cd7837164e899bfa61056392561571
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-22 00:16:26 +0100

    intro to module

--- warehouse/src/main.rs
+++ warehouse/src/main.rs
@@ -1,1 +1,24 @@
+// Intro to Module
+mod inventory {
+    const FLOOR_SPACE: i32 = 10000;
+    const MANAGER: &str = "Ivan Inventory";
+
+    #[derive(Debug)]
+    enum ProductCategory {
+        Ladder,
+        Hammer,
+    }
+
+    #[derive(Debug)]
+    struct Item {
+        name: String,
+        category: ProductCategory,
+        quantity: u32,
+    }
+
+    fn talk_to_manager() {
+        println!("Hey, {MANAGER}, how's your coffee");
+    }
+}
+
 fn main() {}
