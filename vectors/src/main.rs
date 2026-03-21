91e562ce1e5fcaee59103309e2f00e5bd6e8b5e8
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-21 21:38:39 +0100

    vector capacity behind the scenes

--- vectors/src/main.rs
+++ vectors/src/main.rs
@@ -68,21 +68,48 @@
 // }
 
 // ---- Writing Vector Elements ----
+// fn main() {
+//     let pepperoni = String::from("Pepperoni");
+//     let mushroom = String::from("Mushroom");
+//     let sausage = String::from("Sausage");
+//     let mut pizza_toppings = vec![pepperoni, mushroom, sausage];
+
+//     pizza_toppings[1] = String::from("Olives");
+//     println!("{pizza_toppings:#?}");
+
+//     let target_topping = &mut pizza_toppings[2];
+//     target_topping.push_str(" and Meatballs");
+//     let another_topping = &pizza_toppings[1];
+//     let another_one = &pizza_toppings[1];
+
+//     println!("{pizza_toppings:#?}");
+//     println!("{another_topping:#?}");
+//     println!("{another_one:#?}");
+// }
+
+// ---- Vector Capacity Behind the Scenes ----
 fn main() {
-    let pepperoni = String::from("Pepperoni");
-    let mushroom = String::from("Mushroom");
-    let sausage = String::from("Sausage");
-    let mut pizza_toppings = vec![pepperoni, mushroom, sausage];
+    let mut seasons: Vec<&str> = Vec::with_capacity(4);
+    println!(
+        "Length: {}, Capacity: {}",
+        seasons.len(),
+        seasons.capacity()
+    );
 
-    pizza_toppings[1] = String::from("Olives");
-    println!("{pizza_toppings:#?}");
+    seasons.push("Summer");
+    seasons.push("Fall");
+    seasons.push("Winter");
+    seasons.push("Spring");
+    println!(
+        "Length: {}, Capacity: {}",
+        seasons.len(),
+        seasons.capacity()
+    );
 
-    let target_topping = &mut pizza_toppings[2];
-    target_topping.push_str(" and Meatballs");
-    let another_topping = &pizza_toppings[1];
-    let another_one = &pizza_toppings[1];
-
-    println!("{pizza_toppings:#?}");
-    println!("{another_topping:#?}");
-    println!("{another_one:#?}");
+    seasons.push("Summer");
+      println!(
+        "Length: {}, Capacity: {}",
+        seasons.len(),
+        seasons.capacity()
+    );
 }
