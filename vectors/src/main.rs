203e74085752c6ed9c8b7ae839ffca1a7b4245b7
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-21 20:08:58 +0100

    create a vector

--- vectors/src/main.rs
+++ vectors/src/main.rs
@@ -1,2 +1,8 @@
+// ---- Create a Vector ----
 fn main() {
+    let pizza_diameters = vec![8, 10, 12, 14];
+    println!("{pizza_diameters:?}");
+
+    let pastas: Vec<&str> = vec!["Lidia", "Carlota", "Angeles", "Marga"];
+    println!("{pastas:?}");
 }
