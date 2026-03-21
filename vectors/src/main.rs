b490f80b337d4530e7f93c435792543b8e5f7786
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-21 20:59:00 +0100

    the get method

--- vectors/src/main.rs
+++ vectors/src/main.rs
@@ -25,14 +25,29 @@
 // }
 
 // ---- Reading Vector Elements ----
-fn main() {
-    let pizza_diameters = vec![8, 10, 12, 14];
+// fn main() {
+//     let pizza_diameters = vec![8, 10, 12, 14];
 
+//     let pepperoni = String::from("Pepperoni");
+//     let mushroom = String::from("Mushroom");
+//     let sausage = String::from("Sausage");
+//     let pizza_toppings = vec![pepperoni, mushroom, sausage];
+
+//     let pizza_slice = &pizza_toppings[1..3];
+//     println!("{pizza_slice:?}");
+// }
+
+// ---- The get Method ----
+fn main() {
     let pepperoni = String::from("Pepperoni");
     let mushroom = String::from("Mushroom");
     let sausage = String::from("Sausage");
     let pizza_toppings = vec![pepperoni, mushroom, sausage];
 
-    let pizza_slice = &pizza_toppings[1..3];
-    println!("{pizza_slice:?}");
+    let option = pizza_toppings.get(5);
+
+    match option {
+        Some(topping) => println!("The topping is {topping}"),
+        None => println!("No value at that index position"),
+    }
 }
