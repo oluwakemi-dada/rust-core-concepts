955f95fd4c933abf46524cb53d8cf47156d6ba3c
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-22 01:37:06 +0100

    the use keyword

--- warehouse/src/inventory.rs
+++ warehouse/src/inventory.rs
@@ -5,6 +5,4 @@ pub const MANAGER: &str = "Ivan Inventory";
 
 pub fn talk_to_manager() {
     println!("Hey, {}, how's your coffee", MANAGER);
-    println!("{:?}", crate::inventory::products::ProductCategory::Ladder);
-    println!("{:?}", products::ProductCategory::Hammer);
 }
