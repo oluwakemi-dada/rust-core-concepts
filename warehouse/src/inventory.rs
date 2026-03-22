e92ca692b6dc8e1e02b8babbd0efbe4b39a84a89
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-22 01:12:48 +0100

    submodules

--- warehouse/src/inventory.rs
+++ warehouse/src/inventory.rs
@@ -1,19 +1,8 @@
 pub const FLOOR_SPACE: i32 = 10_000;
 pub const MANAGER: &str = "Ivan Inventory";
 
-#[derive(Debug)]
-pub enum ProductCategory {
-    Ladder,
-    Hammer,
-}
-
-#[derive(Debug)]
-pub struct Item {
-    pub name: String,
-    pub category: ProductCategory,
-    pub quantity: u32,
-}
-
 pub fn talk_to_manager() {
     println!("Hey, {MANAGER}, how's your coffee");
 }
+
+pub mod products;
