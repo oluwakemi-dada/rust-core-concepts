78e0accd92d2e16057af7630172fb0ec5ba1c264
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-22 17:12:05 +0100

    create aliases with the as keyword

--- warehouse/src/main.rs
+++ warehouse/src/main.rs
@@ -1,15 +1,14 @@
 mod inventory;
 mod orders;
 
-use inventory::FLOOR_SPACE;
 use inventory::products::{Item, ProductCategory};
+use inventory::{FLOOR_SPACE, MANAGER as INVENTORY_MANAGER};
+use orders::MANAGER as ORDERS_MANAGER;
 
 fn main() {
     println!(
         "Our managers are {} and {}. We have {} square feet of floor space",
-        inventory::MANAGER,
-        orders::MANAGER,
-        FLOOR_SPACE
+        INVENTORY_MANAGER, ORDERS_MANAGER, FLOOR_SPACE
     );
 
     let favorite_category = ProductCategory::Hammer;
