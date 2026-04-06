5846315a2adbac111bb135bf4f34f26ba388f93e
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-06 12:58:17 +0100

    coding challenge

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -1,3 +1,52 @@
+use std::io;
+
+// ----- Coding Challenge -----
+fn make_money(text: &mut String) {
+    text.push_str("$$$");
+}
+
+fn trim_and_capitalize(text: &str) -> String {
+    text.trim().to_uppercase()
+}
+
+fn elements(text: &str) -> Vec<&str> {
+    text.split("!").collect()
+}
+
+fn get_identity() -> String {
+    let mut first_name = String::new();
+    let mut last_name = String::new();
+
+    let input = io::stdin();
+
+    println!("What is your first name?");
+    input
+        .read_line(&mut first_name)
+        .expect("Failed to collect first name");
+
+    println!("What is your last name?");
+    input
+        .read_line(&mut last_name)
+        .expect("Failed to collect last name");
+
+    format!("{} {}", first_name.trim(), last_name.trim())
+}
+
+fn main() {
+    let mut amount = String::from("40");
+    make_money(&mut amount);
+    println!("{amount}");
+
+    let banana = trim_and_capitalize(".      banana   ");
+    println!("{banana}");
+
+    let collection = elements("Gold!Silver!Platinum");
+    println!("{collection:?}");
+
+    let full_name = get_identity();
+    println!("{full_name}")
+}
+
 // ----- Review of Strings -----
 // fn main() {
 //     let pirate = "Bloodhook";
@@ -54,13 +103,13 @@
 // }
 
 // ----- Collecting User Input with read_line Method -----
-use std::io;
+// use std::io;
 
-fn main() {
-    let mut name = String::new();
-    println!("What is your name?");
-    match io::stdin().read_line(&mut name) {
-        Ok(_) => println!("Hello, {}", name.trim()),
-        Err(error) => println!("There was an error: {error}"),
-    }
-}
+// fn main() {
+//     let mut name = String::new();
+//     println!("What is your name?");
+//     match io::stdin().read_line(&mut name) {
+//         Ok(_) => println!("Hello, {}", name.trim()),
+//         Err(error) => println!("There was an error: {error}"),
+//     }
+// }
