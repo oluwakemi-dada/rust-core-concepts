a36743332d0a8f1395ed138e9fd1ed77841cbcb1
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-06 10:14:29 +0100

    review of strings

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -1,1 +1,15 @@
-fn main() {}
+// ----- Review of Strings -----
+fn main() {
+    let pirate = "Bloodhook";
+
+    let empty = String::new();
+
+    let sailor = String::from(pirate);
+
+    let bad_guy = pirate.to_string();
+
+    println!("{pirate} and {sailor} and {bad_guy}");
+
+    let first_initial = &pirate[0..1];
+    println!("{first_initial}")
+}
