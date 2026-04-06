9888a54afcc2848b789ae08b1935c54c751bfdd5
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-06 18:08:47 +0100

    overwriting a value with an existing key

--- hash-maps/src/main.rs
+++ hash-maps/src/main.rs
@@ -47,6 +47,22 @@ use std::collections::HashMap;
 // }
 
 // ----- Access a Value by Key -----
+// fn main() {
+//     let mut coffee_pairings: HashMap<&str, &str> = HashMap::new();
+//     let drink = String::from("Latte");
+//     let milk = String::from("Oat Milk");
+//     coffee_pairings.insert(&drink, &milk);
+//     coffee_pairings.insert("Flat White", "Almond Milk");
+
+//     // let value = coffee_pairings["Flat White"];
+//     let value = coffee_pairings
+//         .get("Flat White")
+//         .copied()
+//         .unwrap_or("Unknown Milk");
+//     println!("{:?}", value)
+// }
+
+// ----- Overwriting a Value with an Existing Key -----
 fn main() {
     let mut coffee_pairings: HashMap<&str, &str> = HashMap::new();
     let drink = String::from("Latte");
@@ -54,10 +70,7 @@ fn main() {
     coffee_pairings.insert(&drink, &milk);
     coffee_pairings.insert("Flat White", "Almond Milk");
 
-    // let value = coffee_pairings["Flat White"];
-    let value = coffee_pairings
-        .get("Flat White")
-        .copied()
-        .unwrap_or("Unknown Milk");
-    println!("{:?}", value)
+    println!("{:?}", coffee_pairings);
+    coffee_pairings.insert("Latte", "Pistachio Milk");
+    println!("{:?}", coffee_pairings);
 }
