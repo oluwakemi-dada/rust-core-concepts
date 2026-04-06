7e8b0f51616e0845131c9d86e2ce53a47cb7d4ed
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-06 10:31:37 +0100

    concatenation

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -1,15 +1,26 @@
 // ----- Review of Strings -----
+// fn main() {
+//     let pirate = "Bloodhook";
+
+//     let empty = String::new();
+
+//     let sailor = String::from(pirate);
+
+//     let bad_guy = pirate.to_string();
+
+//     println!("{pirate} and {sailor} and {bad_guy}");
+
+//     let first_initial = &pirate[0..1];
+//     println!("{first_initial}")
+// }
+
+// ----- Concatenation -----
 fn main() {
-    let pirate = "Bloodhook";
+    let first_name = String::from("Sylvester");
+    let last_name = String::from("Stallone");
 
-    let empty = String::new();
-
-    let sailor = String::from(pirate);
-
-    let bad_guy = pirate.to_string();
-
-    println!("{pirate} and {sailor} and {bad_guy}");
-
-    let first_initial = &pirate[0..1];
-    println!("{first_initial}")
+    let full_name = first_name + &last_name;
+    // let full_name = first_name.clone() + &last_name;
+    println!("{full_name}");
+    println!("{first_name}")
 }
