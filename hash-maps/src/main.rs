86a967b759f707c35df0d31adb44f25cbe76a4d3
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-06 17:53:17 +0100

    hash maps and ownership

--- hash-maps/src/main.rs
+++ hash-maps/src/main.rs
@@ -18,17 +18,30 @@ use std::collections::HashMap;
 // }
 
 // ----- The remove Method -----
+// fn main() {
+//     let data = [("Bobby", 7), ("Grant", 4), ("Ben", 6)];
+
+//     let mut years_at_company = HashMap::from(data);
+//     println!("{:?}", years_at_company);
+
+//     let ben = years_at_company.remove("Ben");
+//     println!("{:?}", ben);
+//     println!("{:?}", ben.unwrap());
+//     println!("{:?}", years_at_company);
+
+//     let ben = years_at_company.remove("Ben");
+//     println!("{:?}", ben);
+// }
+
+// ----- Hash Maps and Ownership -----
 fn main() {
-    let data = [("Bobby", 7), ("Grant", 4), ("Ben", 6)];
+    let mut coffee_pairings: HashMap<&str, &str> = HashMap::new();
+    let drink = String::from("Latte");
+    let milk = String::from("Oat Milk");
+    coffee_pairings.insert(&drink, &milk);
+    coffee_pairings.insert("Flat White", "Almond Milk");
 
-    let mut years_at_company = HashMap::from(data);
-    println!("{:?}", years_at_company);
-
-    let ben = years_at_company.remove("Ben");
-    println!("{:?}", ben);
-    println!("{:?}", ben.unwrap());
-    println!("{:?}", years_at_company);
-
-    let ben = years_at_company.remove("Ben");
-    println!("{:?}", ben);
+    println!("{:?}", coffee_pairings);
+    println!("{}", coffee_pairings.len());
+    println!("{drink} {milk}");
 }
