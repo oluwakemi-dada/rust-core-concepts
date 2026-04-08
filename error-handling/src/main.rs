f0cf7a8c3251431ef5cd0680341f4d4e55e729bd
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-08 13:50:55 +0100

    ask the user for input

--- error-handling/src/main.rs
+++ error-handling/src/main.rs
@@ -21,14 +21,42 @@
 // }
 
 // ----- Opening a File -----
+// use std::fs::File;
+// use std::process;
+
+// fn main() {
+//     let file = match File::open("nonsense.txt") {
+//         Ok(file) => file,
+//         Err(error) => {
+//             eprintln!("Something went wrong reading the file. The error was {error:?}");
+//             process::exit(1)
+//         }
+//     };
+
+//     println!("{file:#?}");
+// }
+
+// ----- Asking the User for Input -----
 use std::fs::File;
+use std::io::stdin;
 use std::process;
 
 fn main() {
-    let file = match File::open("nonsense.txt") {
+    println!("Please enter the name of the file you'd like to read:");
+    let mut input = String::new();
+
+    let user_requested_file = stdin().read_line(&mut input);
+
+    if let Err(error) = user_requested_file {
+        eprintln!("Something went wrong collecting user input. The error was {error:?}");
+        process::exit(1)
+    }
+
+    // .trim returns a string slice automatically
+    let file = match File::open(input.trim()) {
         Ok(file) => file,
         Err(error) => {
-            eprintln!("Something went wrong reading the file. The error was {error:?}");
+            eprintln!("Something went wrong reading the file. The error was {error}");
             process::exit(1)
         }
     };
