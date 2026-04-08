db6ed6bc63b1b6157ee73516d1c107eb2718f311
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-08 12:28:23 +0100

    the process module and the exit function

--- error-handling/src/main.rs
+++ error-handling/src/main.rs
@@ -1,5 +1,14 @@
 // ----- The panic! Macro -----
+// fn main() {
+//     let num = 2;
+//     panic!("Something went wrong ");
+// }
+
+// ----- The process Module and the exit Function -----
+use std::process;
 fn main() {
-    let num = 2;
-    panic!("Something went wrong ");
+    // process::exit(0);
+
+    process::exit(1);
+    println!("This will not print");
 }
