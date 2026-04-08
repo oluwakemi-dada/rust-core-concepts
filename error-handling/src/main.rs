df512014ac200d3a3bdf0c6d4639f2bd169672b9
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-08 14:22:45 +0100

    propagate errors

--- error-handling/src/main.rs
+++ error-handling/src/main.rs
@@ -65,27 +65,73 @@
 // }
 
 // ----- Reading the File's Contents -----
+// use std::fs::File;
+// use std::io::{Read, stdin};
+// use std::process;
+
+// fn main() {
+//     println!("Please enter the name of the file you'd like to read:");
+//     let mut input = String::new();
+
+//     let user_requested_file = stdin().read_line(&mut input);
+
+//     if let Err(error) = user_requested_file {
+//         eprintln!("Something went wrong collecting user input. The error was {error:?}");
+//         process::exit(1)
+//     }
+
+//     // .trim returns a string slice automatically
+//     let mut file = match File::open(input.trim()) {
+//         Ok(file) => file,
+//         Err(error) => {
+//             eprintln!("Something went wrong opening the file. The error was {error}");
+//             process::exit(1)
+//         }
+//     };
+
+//     let mut file_contents = String::new();
+//     let read_operation = file.read_to_string(&mut file_contents);
+
+//     if let Err(error) = read_operation {
+//         eprintln!("Something went wrong reading the file as a string. The error was {error}");
+//         process::exit(1)
+//     }
+
+//     println!("{file:#?}");
+//     println!("{file_contents}");
+// }
+
+// ----- Propagating Errors-----
 use std::fs::File;
-use std::io::{Read, stdin};
+use std::io::{self, Read, stdin};
 use std::process;
 
 fn main() {
+    let file_result = read_file();
+
+    match file_result {
+        Ok(contents) => println!("{contents}"),
+        Err(error) => {
+            eprintln!("There was an errror: {error:?}")
+        }
+    }
+}
+
+fn read_file() -> Result<String, io::Error> {
     println!("Please enter the name of the file you'd like to read:");
     let mut input = String::new();
 
     let user_requested_file = stdin().read_line(&mut input);
 
     if let Err(error) = user_requested_file {
-        eprintln!("Something went wrong collecting user input. The error was {error:?}");
-        process::exit(1)
+        return Err(error);
     }
 
     // .trim returns a string slice automatically
     let mut file = match File::open(input.trim()) {
         Ok(file) => file,
         Err(error) => {
-            eprintln!("Something went wrong opening the file. The error was {error}");
-            process::exit(1)
+            return Err(error);
         }
     };
 
@@ -93,10 +139,8 @@ fn main() {
     let read_operation = file.read_to_string(&mut file_contents);
 
     if let Err(error) = read_operation {
-        eprintln!("Something went wrong reading the file as a string. The error was {error}");
-        process::exit(1)
+        return Err(error);
     }
 
-    println!("{file:#?}");
-    println!("{file_contents}");
+    Ok(file_contents)
 }
