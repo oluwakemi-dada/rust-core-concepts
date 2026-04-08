45d33dfcdb4291bda92f79bf0572f271639640d8
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-08 14:04:22 +0100

    read file's content

--- error-handling/src/main.rs
+++ error-handling/src/main.rs
@@ -37,8 +37,36 @@
 // }
 
 // ----- Asking the User for Input -----
+// use std::fs::File;
+// use std::io::stdin;
+// use std::process;
+
+// fn main() {
+//     println!("Please enter the name of the file you'd like to read:");
+//     let mut input = String::new();
+
+//     let user_requested_file = stdin().read_line(&mut input);
+
+//     if let Err(error) = user_requested_file {
+//         eprintln!("Something went wrong collecting user input. The error was {error:?}");
+//         process::exit(1)
+//     }
+
+//     // .trim returns a string slice automatically
+//     let file = match File::open(input.trim()) {
+//         Ok(file) => file,
+//         Err(error) => {
+//             eprintln!("Something went wrong reading the file. The error was {error}");
+//             process::exit(1)
+//         }
+//     };
+
+//     println!("{file:#?}");
+// }
+
+// ----- Reading the File's Contents -----
 use std::fs::File;
-use std::io::stdin;
+use std::io::{Read, stdin};
 use std::process;
 
 fn main() {
@@ -53,13 +81,22 @@ fn main() {
     }
 
     // .trim returns a string slice automatically
-    let file = match File::open(input.trim()) {
+    let mut file = match File::open(input.trim()) {
         Ok(file) => file,
         Err(error) => {
-            eprintln!("Something went wrong reading the file. The error was {error}");
+            eprintln!("Something went wrong opening the file. The error was {error}");
             process::exit(1)
         }
     };
 
+    let mut file_contents = String::new();
+    let read_operation = file.read_to_string(&mut file_contents);
+
+    if let Err(error) = read_operation {
+        eprintln!("Something went wrong reading the file as a string. The error was {error}");
+        process::exit(1)
+    }
+
     println!("{file:#?}");
+    println!("{file_contents}");
 }
