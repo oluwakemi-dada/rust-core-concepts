39eeaa9fdf2b408574f56f4413d039be69a82e18
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-08 12:46:32 +0100

    standard error - eprintln! macro

--- error-handling/src/main.rs
+++ error-handling/src/main.rs
@@ -5,10 +5,16 @@
 // }
 
 // ----- The process Module and the exit Function -----
-use std::process;
-fn main() {
-    // process::exit(0);
+// use std::process;
+// fn main() {
+//     // process::exit(0);
 
-    process::exit(1);
-    println!("This will not print");
+//     process::exit(1);
+//     println!("This will not print");
+// }
+
+// ----- Standard Error (eprintln! Macro) -----
+fn main() {
+    println!("Some status update");
+    eprintln!("Error: Some error message");
 }
