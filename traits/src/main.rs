f235dc9315ba7ed847eba92778e1755b786eab5b
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-11 23:48:44 +0100

    code refactor

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,118 +1,14 @@
-// ----- Implementing Trait for Struct -----
-// use std::collections::HashMap;
-// use std::fmt::Display;
-
-// trait Accommodation {
-//     fn book(&mut self, name: &str, nights: u32);
-// }
-
-// trait Description {
-//     fn get_description(&self) -> String {
-//         String::from("A wonderful place to stay")
-//     }
-// }
-
-// #[derive(Debug)]
-// struct Hotel<T> {
-//     name: T,
-//     reservations: HashMap<String, u32>,
-// }
-
-// impl<T> Hotel<T> {
-//     fn new(name: T) -> Self {
-//         Self {
-//             name,
-//             reservations: HashMap::new(),
-//         }
-//     }
-// }
-
-// impl<T: Display> Hotel<T> {
-//     fn summarize(&self) -> String {
-//         format!("{}: {}", self.name, self.get_description())
-//     }
-// }
-
-// impl<T> Accommodation for Hotel<T> {
-//     fn book(&mut self, name: &str, nights: u32) {
-//         self.reservations.insert(name.to_string(), nights);
-//     }
-// }
-
-// impl<T> Description for Hotel<T> {}
-
-// #[derive(Debug)]
-// struct AirBnB {
-//     host: String,
-//     guests: Vec<(String, u32)>,
-// }
-
-// impl AirBnB {
-//     fn new(host: &str) -> Self {
-//         Self {
-//             host: host.to_string(),
-//             guests: vec![],
-//         }
-//     }
-// }
-
-// impl Accommodation for AirBnB {
-//     fn book(&mut self, name: &str, nights: u32) {
-//         self.guests.push((name.to_string(), nights));
-//     }
-// }
-
-// impl Description for AirBnB {
-//     fn get_description(&self) -> String {
-//         format!("Please enjoy {}'s apartment", self.host)
-//     }
-// }
-
-// fn book_for_one_night<T: Accommodation + Description>(entity: &mut T, guest: &str) {
-//     entity.book(guest, 1);
-// }
-
-// fn mix_and_match<T, U>(first: &mut T, second: &mut U, guest: &str)
-// where
-//     T: Accommodation + Description,
-//     U: Accommodation,
-// {
-//     first.book(guest, 1);
-//     first.get_description();
-
-//     second.book(guest, 1);
-// }
-
-// fn choose_best_place_to_stay() -> impl Accommodation + Description {
-//     Hotel::new("The Luxe")
-// }
-
-// fn main() {
-//     let mut hotel = Hotel::new(String::from("The Luxe"));
-//     let mut airbnb = AirBnB::new("Peter");
-
-//     let stays: Vec<&dyn Description> = vec![&hotel, &airbnb];
-//     println!("{}", stays[0].get_description());
-//     println!("{}", stays[1].get_description());
-
-//     let mut stays: Vec<&mut dyn Accommodation> = vec![&mut hotel, &mut airbnb];
-//     stays[0].book("Piers", 2);
-//     stays[1].book("Amanda", 3);
-//     println!("{:#?}", hotel);
-//     println!("{:#?}", airbnb);
-// }
-
-// ----- Trait Must be in Scope to Use its Definitions -----
-use std::ops::Add;
-use std::str::FromStr;
+use traits::logging::{Accommodation, AirBnB, Description, Hotel};
+use traits::utils;
 
 fn main() {
-    let a = 5;
-    let b = 10;
+    let mut hotel = Hotel::new(String::from("The Luxe"));
+    println!("{}", hotel.summarize());
+    hotel.book("Dana", 5);
 
-    let sum = a.add(b);
-    println!("{sum}");
+    let mut airbnb = AirBnB::new("Parker");
+    println!("{}", airbnb.get_description());
+    utils::book_for_one_night(&mut airbnb, "Dan");
 
-    let numeric_count = u64::from_str("5");
-    println!("{}", numeric_count.unwrap());
+    utils::mix_and_match(&mut hotel, &mut airbnb, "Phil");
 }
---
+++ traits/src/utils.rs
@@ -0,0 +1,21 @@
+// use super::logging::{Accommodation, Description};
+use crate::logging::{Accommodation, Description};
+
+pub fn book_for_one_night<T: Accommodation + Description>(entity: &mut T, guest: &str) {
+    entity.book(guest, 1);
+}
+
+pub fn mix_and_match<T, U>(first: &mut T, second: &mut U, guest: &str)
+where
+    T: Accommodation + Description,
+    U: Accommodation,
+{
+    first.book(guest, 1);
+    first.get_description();
+
+    second.book(guest, 1);
+}
+
+// fn choose_best_place_to_stay() -> impl Accommodation + Description {
+//     Hotel::new("The Luxe")
+// }
