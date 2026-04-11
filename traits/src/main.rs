2828941553bb906078967aae4056485876c17d3a
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-11 22:58:04 +0100

    trait in scope

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,103 +1,118 @@
-use std::collections::HashMap;
-use std::fmt::Display;
-
 // ----- Implementing Trait for Struct -----
-trait Accommodation {
-    fn book(&mut self, name: &str, nights: u32);
-}
+// use std::collections::HashMap;
+// use std::fmt::Display;
 
-trait Description {
-    fn get_description(&self) -> String {
-        String::from("A wonderful place to stay")
-    }
-}
+// trait Accommodation {
+//     fn book(&mut self, name: &str, nights: u32);
+// }
 
-#[derive(Debug)]
-struct Hotel<T> {
-    name: T,
-    reservations: HashMap<String, u32>,
-}
+// trait Description {
+//     fn get_description(&self) -> String {
+//         String::from("A wonderful place to stay")
+//     }
+// }
 
-impl<T> Hotel<T> {
-    fn new(name: T) -> Self {
-        Self {
-            name,
-            reservations: HashMap::new(),
-        }
-    }
-}
+// #[derive(Debug)]
+// struct Hotel<T> {
+//     name: T,
+//     reservations: HashMap<String, u32>,
+// }
 
-impl<T: Display> Hotel<T> {
-    fn summarize(&self) -> String {
-        format!("{}: {}", self.name, self.get_description())
-    }
-}
+// impl<T> Hotel<T> {
+//     fn new(name: T) -> Self {
+//         Self {
+//             name,
+//             reservations: HashMap::new(),
+//         }
+//     }
+// }
 
-impl<T> Accommodation for Hotel<T> {
-    fn book(&mut self, name: &str, nights: u32) {
-        self.reservations.insert(name.to_string(), nights);
-    }
-}
+// impl<T: Display> Hotel<T> {
+//     fn summarize(&self) -> String {
+//         format!("{}: {}", self.name, self.get_description())
+//     }
+// }
 
-impl<T> Description for Hotel<T> {}
+// impl<T> Accommodation for Hotel<T> {
+//     fn book(&mut self, name: &str, nights: u32) {
+//         self.reservations.insert(name.to_string(), nights);
+//     }
+// }
 
-#[derive(Debug)]
-struct AirBnB {
-    host: String,
-    guests: Vec<(String, u32)>,
-}
+// impl<T> Description for Hotel<T> {}
 
-impl AirBnB {
-    fn new(host: &str) -> Self {
-        Self {
-            host: host.to_string(),
-            guests: vec![],
-        }
-    }
-}
+// #[derive(Debug)]
+// struct AirBnB {
+//     host: String,
+//     guests: Vec<(String, u32)>,
+// }
 
-impl Accommodation for AirBnB {
-    fn book(&mut self, name: &str, nights: u32) {
-        self.guests.push((name.to_string(), nights));
-    }
-}
+// impl AirBnB {
+//     fn new(host: &str) -> Self {
+//         Self {
+//             host: host.to_string(),
+//             guests: vec![],
+//         }
+//     }
+// }
 
-impl Description for AirBnB {
-    fn get_description(&self) -> String {
-        format!("Please enjoy {}'s apartment", self.host)
-    }
-}
+// impl Accommodation for AirBnB {
+//     fn book(&mut self, name: &str, nights: u32) {
+//         self.guests.push((name.to_string(), nights));
+//     }
+// }
 
-fn book_for_one_night<T: Accommodation + Description>(entity: &mut T, guest: &str) {
-    entity.book(guest, 1);
-}
+// impl Description for AirBnB {
+//     fn get_description(&self) -> String {
+//         format!("Please enjoy {}'s apartment", self.host)
+//     }
+// }
 
-fn mix_and_match<T, U>(first: &mut T, second: &mut U, guest: &str)
-where
-    T: Accommodation + Description,
-    U: Accommodation,
-{
-    first.book(guest, 1);
-    first.get_description();
+// fn book_for_one_night<T: Accommodation + Description>(entity: &mut T, guest: &str) {
+//     entity.book(guest, 1);
+// }
 
-    second.book(guest, 1);
-}
+// fn mix_and_match<T, U>(first: &mut T, second: &mut U, guest: &str)
+// where
+//     T: Accommodation + Description,
+//     U: Accommodation,
+// {
+//     first.book(guest, 1);
+//     first.get_description();
 
-fn choose_best_place_to_stay() -> impl Accommodation + Description {
-    Hotel::new("The Luxe")
-}
+//     second.book(guest, 1);
+// }
+
+// fn choose_best_place_to_stay() -> impl Accommodation + Description {
+//     Hotel::new("The Luxe")
+// }
+
+// fn main() {
+//     let mut hotel = Hotel::new(String::from("The Luxe"));
+//     let mut airbnb = AirBnB::new("Peter");
+
+//     let stays: Vec<&dyn Description> = vec![&hotel, &airbnb];
+//     println!("{}", stays[0].get_description());
+//     println!("{}", stays[1].get_description());
+
+//     let mut stays: Vec<&mut dyn Accommodation> = vec![&mut hotel, &mut airbnb];
+//     stays[0].book("Piers", 2);
+//     stays[1].book("Amanda", 3);
+//     println!("{:#?}", hotel);
+//     println!("{:#?}", airbnb);
+// }
+
+// ----- Trait Must be in Scope to Use its Definitions -----
+use std::ops::Add;
+use std::str::FromStr;
 
 fn main() {
-    let mut hotel = Hotel::new(String::from("The Luxe"));
-    let mut airbnb = AirBnB::new("Peter");
+    let a = 5;
+    let b = 10;
 
-    let stays: Vec<&dyn Description> = vec![&hotel, &airbnb];
-    println!("{}", stays[0].get_description());
-    println!("{}", stays[1].get_description());
+    let sum = a.add(b);
+    println!("{sum}");
 
-    let mut stays: Vec<&mut dyn Accommodation> = vec![&mut hotel, &mut airbnb];
-    stays[0].book("Piers", 2);
-    stays[1].book("Amanda", 3);
-    println!("{:#?}", hotel);
-    println!("{:#?}", airbnb);
+    let numeric_count = u64::from_str("5");
+    println!("{}", numeric_count.unwrap());
 }
