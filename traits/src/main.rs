dca2ab4f8849256d78d309ac3c493940d8e5fa7b
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-13 19:33:25 +0100

    implementing the display trait on an enum

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,23 +1,43 @@
 use std::fmt::{Display, Formatter, Result};
 
+enum AppleType {
+    RedDelicious,
+    GrannySmith,
+}
+
+impl Display for AppleType {
+    fn fmt(&self, formatter: &mut Formatter) -> Result {
+        match self {
+            AppleType::RedDelicious => write!(formatter, "🍎 Delicious 🍎"),
+            AppleType::GrannySmith => write!(formatter, "🍏 Granny Smith 🍏"),
+        }
+    }
+}
+
 struct Apple {
-    kind: String,
+    kind: AppleType,
     price: f64,
 }
 
 impl Display for Apple {
-    fn fmt(&self, formatter: &mut Formatter<'_>) -> Result {
-        write!(formatter, "{} 🍏 for {}", self.kind, self.price)
+    fn fmt(&self, formatter: &mut Formatter) -> Result {
+        write!(formatter, "{} for {}", self.kind, self.price)
     }
 }
 
 fn main() {
     let lunch_snack = Apple {
-        kind: String::from("Granny Smith"),
+        kind: AppleType::GrannySmith,
         price: 1.04,
     };
 
+    let dinner_snack = Apple {
+        kind: AppleType::RedDelicious,
+        price: 1.15,
+    };
+
     println!("{}", lunch_snack);
+    println!("{}", dinner_snack);
 }
 
 // -------------------------------------------------------------- //
