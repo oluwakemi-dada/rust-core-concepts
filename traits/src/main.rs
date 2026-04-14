7aa2dd5246ed2088c6c74f21946fc66e10b51538
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-14 12:18:51 +0100

    associated types I

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,56 +1,80 @@
-use std::cmp::Ordering;
+use std::ops::Add;
 
-struct Job {
-    salary: u32,
-    commute_time: u32,
+#[derive(Debug)]
+struct Lunch {
+    cost: f64,
 }
 
-impl PartialEq for Job {
-    fn eq(&self, other: &Self) -> bool {
-        self.salary == other.salary
-    }
-}
+impl Add for Lunch {
+    type Output = Lunch;
 
-impl Eq for Job {}
-
-impl PartialOrd for Job {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        self.salary.partial_cmp(&other.salary)
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            cost: self.cost + rhs.cost,
+        }
     }
 }
 
 fn main() {
-    let long_commute_job = Job {
-        salary: 100000,
-        commute_time: 2,
-    };
-
-    let short_commute_job = Job {
-        salary: 75000,
-        commute_time: 1,
-    };
-
-    println!("{}", long_commute_job > short_commute_job);
-    println!("{}", long_commute_job < short_commute_job);
-    println!("{}", long_commute_job == short_commute_job);
-    println!("{}", long_commute_job >= short_commute_job);
-    println!("{}", long_commute_job <= short_commute_job);
-
-    let new_opportunity = Job {
-        salary: 100000,
-        commute_time: 1,
-    };
-
-    println!("{}", long_commute_job > new_opportunity);
-    println!("{}", long_commute_job < new_opportunity);
-
-    println!("{}", long_commute_job == new_opportunity);
-    println!("{}", long_commute_job != new_opportunity);
-
-    println!("{}", long_commute_job >= new_opportunity);
-    println!("{}", long_commute_job <= new_opportunity);
+    let one = Lunch { cost: 19.99 };
+    let two = Lunch { cost: 29.99 };
+    println!("{:?}", one + two);
 }
 
+// -------------------------------------------------------------- //
+
+// use std::cmp::Ordering;
+
+// struct Job {
+//     salary: u32,
+//     commute_time: u32,
+// }
+
+// impl PartialEq for Job {
+//     fn eq(&self, other: &Self) -> bool {
+//         self.salary == other.salary
+//     }
+// }
+
+// impl Eq for Job {}
+
+// impl PartialOrd for Job {
+//     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+//         self.salary.partial_cmp(&other.salary)
+//     }
+// }
+
+// fn main() {
+//     let long_commute_job = Job {
+//         salary: 100000,
+//         commute_time: 2,
+//     };
+
+//     let short_commute_job = Job {
+//         salary: 75000,
+//         commute_time: 1,
+//     };
+
+//     println!("{}", long_commute_job > short_commute_job);
+//     println!("{}", long_commute_job < short_commute_job);
+//     println!("{}", long_commute_job == short_commute_job);
+//     println!("{}", long_commute_job >= short_commute_job);
+//     println!("{}", long_commute_job <= short_commute_job);
+
+//     let new_opportunity = Job {
+//         salary: 100000,
+//         commute_time: 1,
+//     };
+
+//     println!("{}", long_commute_job > new_opportunity);
+//     println!("{}", long_commute_job < new_opportunity);
+
+//     println!("{}", long_commute_job == new_opportunity);
+//     println!("{}", long_commute_job != new_opportunity);
+
+//     println!("{}", long_commute_job >= new_opportunity);
+//     println!("{}", long_commute_job <= new_opportunity);
+// }
 
 // -------------------------------------------------------------- //
 
