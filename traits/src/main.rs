1183909d486851d62e2e30c2336ebfca63359b99
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-14 12:51:27 +0100

    associated types II

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,25 +1,40 @@
 use std::ops::Add;
 
-#[derive(Debug)]
-struct Lunch {
-    cost: f64,
-}
-
-impl Add for Lunch {
-    type Output = Lunch;
-
-    fn add(self, rhs: Self) -> Self::Output {
-        Self {
-            cost: self.cost + rhs.cost,
-        }
-    }
+fn add_two_numbers<T: Add<Output = T>>(a: T, b: T) -> T {
+    a + b
 }
 
 fn main() {
-    let one = Lunch { cost: 19.99 };
-    let two = Lunch { cost: 29.99 };
-    println!("{:?}", one + two);
+    let integer_sum = add_two_numbers(1, 2);
+    let float_sum = add_two_numbers(1.5, 2.4);
+    println!("{integer_sum} and {float_sum}");
 }
+
+
+// -------------------------------------------------------------- //
+
+// use std::ops::Add;
+
+// #[derive(Debug)]
+// struct Lunch {
+//     cost: f64,
+// }
+
+// impl Add for Lunch {
+//     type Output = Lunch;
+
+//     fn add(self, rhs: Self) -> Self::Output {
+//         Self {
+//             cost: self.cost + rhs.cost,
+//         }
+//     }
+// }
+
+// fn main() {
+//     let one = Lunch { cost: 19.99 };
+//     let two = Lunch { cost: 29.99 };
+//     println!("{:?}", one + two);
+// }
 
 // -------------------------------------------------------------- //
 
