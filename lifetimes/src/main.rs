3b821a86d936cd692a3a22a7a0ed86fd735e1e70
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-15 21:02:36 +0100

    references as function parameters

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -39,24 +39,49 @@
 
 // ---------------------------------------------------- //
 
-fn main() {
-    // let some_cities = {
-    //     let cities = vec![
-    //         String::from("London"),
-    //         String::from("New York"),
-    //         String::from("Barcelona"),
-    //     ];
-    //     // Invalid line
-    //     &cities[..2]
-    // };
+// fn main() {
+//     // let some_cities = {
+//     //     let cities = vec![
+//     //         String::from("London"),
+//     //         String::from("New York"),
+//     //         String::from("Barcelona"),
+//     //     ];
+//     //     // Invalid line
+//     //     &cities[..2]
+//     // };
 
+//     let cities = vec![
+//         String::from("London"),
+//         String::from("New York"),
+//         String::from("Barcelona"),
+//     ];
+
+//     let favorite_cities = &cities[0..2];
+//     println!("{favorite_cities:?}");
+//     let places = cities;
+// }
+
+// ---------------------------------------------------- //
+
+fn select_first_two_elements(items: &[String]) -> &[String] {
+    &items[..2]
+}
+
+fn main() {
     let cities = vec![
         String::from("London"),
         String::from("New York"),
         String::from("Barcelona"),
     ];
+    let two_cities = select_first_two_elements(&cities);
+    println!("{two_cities:?}");
 
-    let favorite_cities = &cities[0..2];
-    println!("{favorite_cities:?}");
-    let places = cities;
+    {
+        let coffees = [String::from("Latte"), String::from("Mocha")];
+        let two_coffees = select_first_two_elements(&coffees);
+        println!("{two_coffees:?}");
+    }
 }
+
+
+
