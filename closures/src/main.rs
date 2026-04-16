4e48dac6dfa0e6bd45bbf2c87f37c60cbffc079f
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-16 20:33:19 +0100

    closure shortcuts

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -1,19 +1,33 @@
+// fn main() {
+//     let multiplier = 5;
+
+//     // fn multiply_by(value: i32) -> i32 {
+//     //     value * multiplier
+//     // }
+
+//     let multiply_by = |value: i32| -> i32 { return multiplier * value };
+
+//     println!("{}", multiply_by(2));
+
+//     let product = |a: i32, b: i32| -> i32 {
+//         println!("Calculating product for you");
+//         a * b
+//     };
+
+//     println!("{}", product(3, 9));
+//     println!("{}", product(5, 8));
+// }
+
+// --------------------------------------------------- //
 fn main() {
     let multiplier = 5;
 
-    // fn multiply_by(value: i32) -> i32 {
-    //     value * multiplier
-    // }
+    let multiply_by = |value| value * multiplier;
+    println!("{}", multiply_by(3 as u8));
 
-    let multiply_by = |value: i32| -> i32 { return multiplier * value };
+    let mirror = |value| value;
+    println!("{}", mirror(10));
 
-    println!("{}", multiply_by(2));
-
-    let product = |a: i32, b: i32| -> i32 {
-        println!("Calculating product for you");
-        a * b
-    };
-
-    println!("{}", product(3, 9));
-    println!("{}", product(5, 8));
+    // Invalid due to inference
+    // println!("{}", mirror("Why"));
 }
