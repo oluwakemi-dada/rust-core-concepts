44903d396c95ae9d604fd4b4a89cb41f5f63a71d
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-16 12:05:24 +0100

    lifetimes and referents

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -63,7 +63,28 @@
 
 // ---------------------------------------------------- //
 
-fn select_first_two_elements(items: &[String]) -> &[String] {
+// fn select_first_two_elements(items: &[String]) -> &[String] {
+//     &items[..2]
+// }
+
+// fn main() {
+//     let cities = vec![
+//         String::from("London"),
+//         String::from("New York"),
+//         String::from("Barcelona"),
+//     ];
+//     let two_cities = select_first_two_elements(&cities);
+//     println!("{two_cities:?}");
+
+//     {
+//         let coffees = [String::from("Latte"), String::from("Mocha")];
+//         let two_coffees = select_first_two_elements(&coffees);
+//         println!("{two_coffees:?}");
+//     }
+// }
+
+// ---------------------------------------------------- //
+fn select_first_two_elements<'a>(items: &'a [String]) -> &'a [String] {
     &items[..2]
 }
 
@@ -73,7 +94,10 @@ fn main() {
         String::from("New York"),
         String::from("Barcelona"),
     ];
-    let two_cities = select_first_two_elements(&cities);
+    let two_cities = {
+        let cities_ref = &cities;
+        select_first_two_elements(cities_ref)
+    };
     println!("{two_cities:?}");
 
     {
@@ -82,6 +106,5 @@ fn main() {
         println!("{two_coffees:?}");
     }
 }
-
 
 
