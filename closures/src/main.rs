cb4e4e6da6c7c43687657ef9fc345e0888128a0b
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-16 20:20:10 +0100

    intro to closures

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -1,1 +1,19 @@
-fn main() {}
+fn main() {
+    let multiplier = 5;
+
+    // fn multiply_by(value: i32) -> i32 {
+    //     value * multiplier
+    // }
+
+    let multiply_by = |value: i32| -> i32 { return multiplier * value };
+
+    println!("{}", multiply_by(2));
+
+    let product = |a: i32, b: i32| -> i32 {
+        println!("Calculating product for you");
+        a * b
+    };
+
+    println!("{}", product(3, 9));
+    println!("{}", product(5, 8));
+}
