1a2854d130719195ef03cf567c014da87816c431
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-16 12:52:08 +0100

    lifetime elision rules I

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -84,8 +84,39 @@
 // }
 
 // ---------------------------------------------------- //
-fn select_first_two_elements<'a>(items: &'a [String]) -> &'a [String] {
-    &items[..2]
+
+// fn select_first_two_elements<'a>(items: &'a [String]) -> &'a [String] {
+//     &items[..2]
+// }
+
+// fn main() {
+//     let cities = vec![
+//         String::from("London"),
+//         String::from("New York"),
+//         String::from("Barcelona"),
+//     ];
+//     let two_cities = {
+//         let cities_ref = &cities;
+//         select_first_two_elements(cities_ref)
+//     };
+//     println!("{two_cities:?}");
+
+//     {
+//         let coffees = [String::from("Latte"), String::from("Mocha")];
+//         let two_coffees = select_first_two_elements(&coffees);
+//         println!("{two_coffees:?}");
+//     }
+// }
+
+// ---------------------------------------------------- //
+
+
+fn my_awesome_function(first: &i32, second: String) -> &i32 {
+    first
+}
+
+fn select_first_two_elements(items: &[String]) -> &[String] {
+    &items[0..2]
 }
 
 fn main() {
@@ -106,5 +137,4 @@ fn main() {
         println!("{two_coffees:?}");
     }
 }
-
 
