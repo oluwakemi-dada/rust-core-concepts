b9f3d57c15b36838ca4685cc894ae3b2aa0fb237
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-17 15:34:59 +0100

    method that accepts a closure

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -84,16 +84,56 @@
 
 // --------------------------------------------------- //
 
-fn main() {
-    let option = Some("Salami");
-    let closure = || "Pizza";
-    let food = option.unwrap_or_else(closure);
-    println!("{food}");
+// fn main() {
+//     let option = Some("Salami");
+//     let closure = || "Pizza";
+//     let food = option.unwrap_or_else(closure);
+//     println!("{food}");
 
-    let option: Option<&str> = None;
-    let pizza_fan = false;
-    let closure = || if pizza_fan { "Pizza" } else { "Hot Pockets" };
-    let food = option.unwrap_or_else(closure);
-    println!("{food}");
+//     let option: Option<&str> = None;
+//     let pizza_fan = false;
+//     let closure = || if pizza_fan { "Pizza" } else { "Hot Pockets" };
+//     let food = option.unwrap_or_else(closure);
+//     println!("{food}");
+// }
+
+// --------------------------------------------------- //
+use std::io::stdin;
+
+#[derive(Debug)]
+struct Vault {
+    password: String,
+    treasure: String,
 }
 
+impl Vault {
+    fn unlock<F>(self, procedure: F) -> Option<String>
+    where
+        F: FnOnce() -> String,
+    {
+        let user_password = procedure();
+        if user_password == self.password {
+            Some(self.treasure)
+        } else {
+            None
+        }
+    }
+}
+
+fn main() {
+    let vault = Vault {
+        password: String::from("topsecret"),
+        treasure: String::from("Gold"),
+    };
+
+    let hack = || {
+        let mut user_input = String::new();
+        println!("Please provide a password to crack the vault");
+        stdin().read_line(&mut user_input);
+        user_input.trim().to_string()
+    };
+
+    let extraction = vault.unlock(hack);
+
+    println!("{:?}", extraction);
+}
