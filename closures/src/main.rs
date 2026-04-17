58f99a9626bc36aaa829486b4df8ca2aa57810e8
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-17 20:18:15 +0100

    coding challenge

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -1,3 +1,70 @@
+#[derive(Debug)]
+struct SupermarketItem {
+    name: String,
+    price: f64,
+}
+
+#[derive(Debug)]
+struct ShoppingCart {
+    items: Vec<SupermarketItem>,
+}
+
+impl ShoppingCart {
+    fn traverse_items<F>(&mut self, mut operation: F)
+    where
+        F: FnMut(&mut SupermarketItem),
+    {
+        let final_index = self.items.len() - 1;
+        let mut current_index = 0;
+        while current_index <= final_index {
+            let current_item = &mut self.items[current_index];
+            operation(current_item);
+            current_index += 1
+        }
+    }
+
+    fn checkout<F>(self, operation: F)
+    where
+        F: FnOnce(Self),
+    {
+        operation(self)
+    }
+}
+
+fn main() {
+    let mut shopping_cart = ShoppingCart {
+        items: vec![
+            SupermarketItem {
+                name: String::from("APPLE"),
+                price: 3.99,
+            },
+            SupermarketItem {
+                name: String::from("BANANA"),
+                price: 2.99,
+            },
+        ],
+    };
+
+    shopping_cart.traverse_items(|item| {
+        item.price *= 0.85;
+    });
+
+    shopping_cart.traverse_items(|item| {
+        item.name = item.name.to_lowercase();
+    });
+
+    let mut total_price = 0.0;
+    shopping_cart.checkout(|mut cart| {
+        println!("{:#?}", cart);
+
+        cart.traverse_items(|item| total_price += item.price);
+    });
+
+    println!("${:.2}", total_price);
+}
+
+// --------------------------------------------------- //
+
 // fn main() {
 //     let multiplier = 5;
 
@@ -237,24 +304,23 @@
 
 // --------------------------------------------------- //
 
-fn execute_thrice<F>(mut procedure: F)
-where
-    F: FnMut(),
-{
-    procedure();
-    procedure();
-    procedure();
-}
+// fn execute_thrice<F>(mut procedure: F)
+// where
+//     F: FnMut(),
+// {
+//     procedure();
+//     procedure();
+//     procedure();
+// }
 
-fn bake_cake() {
-    println!("Hello chocolate cake");
-}
+// fn bake_cake() {
+//     println!("Hello chocolate cake");
+// }
 
-fn main() {
-    execute_thrice(bake_cake);
+// fn main() {
+//     execute_thrice(bake_cake);
 
-    let option: Option<Vec<String>> = None;
-    let collection = option.unwrap_or_else(Vec::new);
-    println!("{:?}", collection);
-}
-
+//     let option: Option<Vec<String>> = None;
+//     let collection = option.unwrap_or_else(Vec::new);
+//     println!("{:?}", collection);
+// }
