81099cee1ff428cccdad124ac14c5c4f822170c0
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-18 22:37:40 +0100

    the zip method

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -448,13 +448,31 @@
 
 // --------------------------------------------------------- //
 
+// fn main() {
+//     let numbers = [4, 8, 15, 16, 23, 42];
+
+//     let (evens, odds): (Vec<i32>, Vec<i32>) =
+//         numbers.into_iter().partition(|number| number % 2 == 0);
+
+//     // println!("{groups:?}");
+//     println!("{evens:?}");
+//     println!("{odds:?}");
+// }
+
+// --------------------------------------------------------- //
+
 fn main() {
-    let numbers = [4, 8, 15, 16, 23, 42];
+    let first_names = ["Casey", "Robert", "Cargo", "Dan"];
+    let last_names = ["Johnson", "Smith", "Rustman"];
 
-    let (evens, odds): (Vec<i32>, Vec<i32>) =
-        numbers.into_iter().partition(|number| number % 2 == 0);
+    for (first_name, last_name) in first_names.iter().zip(last_names) {
+        println!("{first_name} {last_name}");
+    }
 
-    // println!("{groups:?}");
-    println!("{evens:?}");
-    println!("{odds:?}");
+    let complete_names = first_names
+        .iter()
+        .zip(last_names)
+        .map(|(first_name, last_name)| format!("{first_name} {last_name}"))
+        .collect::<Vec<String>>();
+    println!("{complete_names:#?}");
 }
