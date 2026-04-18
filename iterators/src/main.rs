b86a2a46d47063d4831d587bbb5aaef105b81fd2
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-18 21:35:09 +0100

    the cloned method

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -299,60 +299,77 @@
 
 // --------------------------------------------------------- //
 
-#[derive(Debug, PartialEq, Eq)]
-enum ChannelType {
-    Comedy,
-    News,
-    ProgrammingTutorials,
-}
+// #[derive(Debug, PartialEq, Eq)]
+// enum ChannelType {
+//     Comedy,
+//     News,
+//     ProgrammingTutorials,
+// }
 
-#[derive(Debug)]
-struct TVChannel {
-    name: String,
-    channel_type: ChannelType,
-}
+// #[derive(Debug)]
+// struct TVChannel {
+//     name: String,
+//     channel_type: ChannelType,
+// }
+
+// fn main() {
+//     let channels = [
+//         TVChannel {
+//             name: String::from("CBS"),
+//             channel_type: ChannelType::Comedy,
+//         },
+//         TVChannel {
+//             name: String::from("RustLive"),
+//             channel_type: ChannelType::ProgrammingTutorials,
+//         },
+//         TVChannel {
+//             name: String::from("NBC"),
+//             channel_type: ChannelType::News,
+//         },
+//         TVChannel {
+//             name: String::from("RustTV"),
+//             channel_type: ChannelType::ProgrammingTutorials,
+//         },
+//     ];
+
+//     let all_are_rust = channels
+//         .iter()
+//         .all(|channel| channel.channel_type == ChannelType::ProgrammingTutorials);
+
+//     let any_are_rust = channels
+//         .iter()
+//         .any(|channel| channel.channel_type == ChannelType::ProgrammingTutorials);
+
+//     println!("{all_are_rust}");
+//     println!("{any_are_rust}");
+
+//     // let good_channels: Vec<String> = channels
+//     //     .iter()
+//     //     .filter(|channel| channel.channel_type == ChannelType::ProgrammingTutorials)
+//     //     .map(|channel| channel.name.clone())
+//     //     .collect();
+
+//     // println!("{:#?}", good_channels.len() == channels.len());
+
+//     // let good_channel = channels
+//     //     .iter()
+//     //     .find(|channel| channel.channel_type == ChannelType::ProgrammingTutorials);
+//     // println!("{}", good_channel.is_some());
+// }
+
+// --------------------------------------------------------- //
 
 fn main() {
-    let channels = [
-        TVChannel {
-            name: String::from("CBS"),
-            channel_type: ChannelType::Comedy,
-        },
-        TVChannel {
-            name: String::from("RustLive"),
-            channel_type: ChannelType::ProgrammingTutorials,
-        },
-        TVChannel {
-            name: String::from("NBC"),
-            channel_type: ChannelType::News,
-        },
-        TVChannel {
-            name: String::from("RustTV"),
-            channel_type: ChannelType::ProgrammingTutorials,
-        },
+    let teas = [
+        String::from("Hot Earl Grey"),
+        String::from("Iced Green"),
+        String::from("Hot Matcha"),
     ];
 
-    let all_are_rust = channels
+    let more_teas: Vec<String> = teas
         .iter()
-        .all(|channel| channel.channel_type == ChannelType::ProgrammingTutorials);
-
-    let any_are_rust = channels
-        .iter()
-        .any(|channel| channel.channel_type == ChannelType::ProgrammingTutorials);
-
-    println!("{all_are_rust}");
-    println!("{any_are_rust}");
-
-    // let good_channels: Vec<String> = channels
-    //     .iter()
-    //     .filter(|channel| channel.channel_type == ChannelType::ProgrammingTutorials)
-    //     .map(|channel| channel.name.clone())
-    //     .collect();
-
-    // println!("{:#?}", good_channels.len() == channels.len());
-
-    // let good_channel = channels
-    //     .iter()
-    //     .find(|channel| channel.channel_type == ChannelType::ProgrammingTutorials);
-    // println!("{}", good_channel.is_some());
+        .filter(|tea| tea.contains("Hot"))
+        .cloned()
+        .collect();
+    println!("{more_teas:#?}");
 }
