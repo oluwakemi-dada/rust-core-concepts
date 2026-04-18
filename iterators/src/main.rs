deb2988c948baa8fa3ded3242125f34e5f59beed
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-18 22:00:36 +0100

    the flat_map method

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -401,9 +401,27 @@
 
 // --------------------------------------------------------- //
 
+// fn main() {
+//     let spreadsheet = vec![[100, 200, 300], [123, 456, 789], [987, 654, 321]];
+
+//     let values: Vec<i32> = spreadsheet.into_iter().flatten().collect();
+//     println!("{:?}", values);
+// }
+
+// --------------------------------------------------------- //
+
 fn main() {
-    let spreadsheet = vec![[100, 200, 300], [123, 456, 789], [987, 654, 321]];
-    
-    let values: Vec<i32> = spreadsheet.into_iter().flatten().collect();
-    println!("{:?}", values);
+    let attendees = [
+        "Bob, Mary, Kevin",
+        "Mike, Robbie, Matt, Austin",
+        "Piers, Liam",
+    ];
+
+    let attendees: Vec<&str> = attendees
+        .iter()
+        .flat_map(|group| group.split(", "))
+        .collect();
+
+    println!("{attendees:#?}");
+
 }
