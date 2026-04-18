64774e2b4e0c5bfaf52a325d40baade895fd6da5
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-18 14:06:34 +0100

    the intoiterator trait

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -1,7 +1,21 @@
-fn main() {
-    let numbers = vec![4, 8, 15, 16, 23, 42];
+// fn main() {
+//     let numbers = vec![4, 8, 15, 16, 23, 42];
 
-    for number in numbers {
-        println!("{number}")
-    }
+//     for number in numbers {
+//         println!("{number}")
+//     }
+// }
+
+// --------------------------------------------------------- //
+use std::{collections::HashMap, hash::Hash};
+fn main() {
+    let my_vector = vec![4, 8, 15, 16, 23, 42];
+    let my_iterator = my_vector.into_iter();
+
+    let my_vector = vec![false, true, false, true];
+    let my_iterator = my_vector.into_iter();
+
+    let mut my_hashmap = HashMap::new();
+    my_hashmap.insert("CBS", 2);
+    let my_iterator  = my_hashmap.into_iter();
 }
