126c8a36123363f21d56ae57e9872b9e4930d6cc
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-18 15:01:01 +0100

    the for loop with iterator

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -23,20 +23,32 @@
 
 // --------------------------------------------------------- //
 
+// fn main() {
+//     let my_vector = vec![4, 8, 15, 16, 23, 42];
+//     let mut my_iterator = my_vector.into_iter();
+
+//     println!("{:?}", my_iterator);
+
+//     println!("{:?}", my_iterator.next());
+//     println!("{:?}", my_iterator.next());
+//     println!("{:?}", my_iterator.next());
+//     println!("{:?}", my_iterator.next());
+//     println!("{:?}", my_iterator.next());
+//     println!("{:?}", my_iterator.next());
+//     println!("{:?}", my_iterator.next());
+//     println!("{:?}", my_iterator.next());
+
+//     println!("{:?}", my_iterator);
+// }
+
+// --------------------------------------------------------- //
 fn main() {
     let my_vector = vec![4, 8, 15, 16, 23, 42];
-    let mut my_iterator = my_vector.into_iter();
+    // let  my_iterator = my_vector.into_iter();
 
-    println!("{:?}", my_iterator);
+    for number in my_vector {
+      println!("{number}");
+    }
 
-    println!("{:?}", my_iterator.next());
-    println!("{:?}", my_iterator.next());
-    println!("{:?}", my_iterator.next());
-    println!("{:?}", my_iterator.next());
-    println!("{:?}", my_iterator.next());
-    println!("{:?}", my_iterator.next());
-    println!("{:?}", my_iterator.next());
-    println!("{:?}", my_iterator.next());
-
-    println!("{:?}", my_iterator);
+    // println!("{:?}", my_vector);
 }
