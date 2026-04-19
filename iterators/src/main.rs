a6f1fd15b1e3db825f2f61d36148793b940e3c4f
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-19 22:22:12 +0100

    the sum, product, max, min, and count methods

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -521,28 +521,65 @@
 
 // --------------------------------------------------------- //
 
+// fn main() {
+//     let earnings = [4, 7, 9, 13];
+
+//     let sum = earnings
+//         .into_iter()
+//         .reduce(|total, current| total + current);
+
+//     println!("{sum:?}");
+
+//     let address_portions = [
+//         String::from("123 Elm Street"),
+//         String::from("Suburbia"),
+//         String::from("New Jersey"),
+//     ];
+//     println!("{}", address_portions.join(", "));
+
+//     let address = address_portions
+//         .into_iter()
+//         .reduce(|mut accumulator, portion| {
+//             accumulator.push_str(", ");
+//             accumulator.push_str(&portion);
+//             accumulator
+//         });
+//     println!("{address:?}");
+// }
+
+// --------------------------------------------------------- //
+
 fn main() {
-    let earnings = [4, 7, 9, 13];
+    let numbers = vec![4, 8, 15, 16, 23, 42];
 
-    let sum = earnings
-        .into_iter()
-        .reduce(|total, current| total + current);
+    let total: i32 = numbers.iter().sum();
+    println!("{total}");
 
-    println!("{sum:?}");
+    let product: i32 = numbers.iter().product();
+    println!("{product}");
 
-    let address_portions = [
-        String::from("123 Elm Street"),
-        String::from("Suburbia"),
-        String::from("New Jersey"),
-    ];
-    println!("{}", address_portions.join(", "));
+    let max = numbers.iter().max().unwrap();
+    println!("{max}");
 
-    let address = address_portions
-        .into_iter()
-        .reduce(|mut accumulator, portion| {
-            accumulator.push_str(", ");
-            accumulator.push_str(&portion);
-            accumulator
-        });
-    println!("{address:?}");
+    let min = numbers.iter().min().unwrap();
+    println!("{min}");
+
+    let count = numbers.iter().count();
+    println!("{count}");
+
+    let numbers = vec![4.6, 8.8, 0.0 / 0.0, 6.2, f64::NAN];
+    
+    let sum = numbers
+        .iter()
+        .filter(|number| !number.is_nan())
+        .copied()
+        .fold(0.0, |total, current| total + current);
+    println!("{sum}");
+
+    let max = numbers
+        .iter()
+        .filter(|number| !number.is_nan())
+        .copied()
+        .reduce(|accum, current| accum.max(current));
+    println!("{max:?}");
 }
