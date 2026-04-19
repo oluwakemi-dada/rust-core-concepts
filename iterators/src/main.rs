a16b3a29169cf0b4d9ee0929b6f44e32907a8a5c
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-19 20:59:20 +0100

    the reduce method

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -478,42 +478,71 @@
 // }
 
 // --------------------------------------------------------- //
-use std::collections::HashMap;
 
-struct SupportStaff {
-    day: String,
-    employee: String,
-}
+// use std::collections::HashMap;
+
+// struct SupportStaff {
+//     day: String,
+//     employee: String,
+// }
+
+// fn main() {
+//     let earnings = [4, 7, 9, 13];
+
+//     let sum = earnings.into_iter().fold(0, |total, current| {
+//         println!("Toatal: {total}, current: {current}");
+//         total + current
+//     });
+//     println!("{sum}");
+
+//     let week = [
+//         SupportStaff {
+//             day: String::from("Monday"),
+//             employee: String::from("Brian"),
+//         },
+//         SupportStaff {
+//             day: String::from("Tuesday"),
+//             employee: String::from("Cam"),
+//         },
+//         SupportStaff {
+//             day: String::from("Wednesday"),
+//             employee: String::from("Walter"),
+//         },
+//     ];
+
+//     let map = week
+//         .into_iter()
+//         .fold(HashMap::new(), |mut data, entry: SupportStaff| {
+//             data.insert(entry.day, entry.employee);
+//             data
+//         });
+//     println!("{map:?}");
+// }
+
+// --------------------------------------------------------- //
 
 fn main() {
     let earnings = [4, 7, 9, 13];
 
-    let sum = earnings.into_iter().fold(0, |total, current| {
-        println!("Toatal: {total}, current: {current}");
-        total + current
-    });
-    println!("{sum}");
-
-    let week = [
-        SupportStaff {
-            day: String::from("Monday"),
-            employee: String::from("Brian"),
-        },
-        SupportStaff {
-            day: String::from("Tuesday"),
-            employee: String::from("Cam"),
-        },
-        SupportStaff {
-            day: String::from("Wednesday"),
-            employee: String::from("Walter"),
-        },
-    ];
-
-    let map = week
+    let sum = earnings
         .into_iter()
-        .fold(HashMap::new(), |mut data, entry: SupportStaff| {
-            data.insert(entry.day, entry.employee);
-            data
+        .reduce(|total, current| total + current);
+
+    println!("{sum:?}");
+
+    let address_portions = [
+        String::from("123 Elm Street"),
+        String::from("Suburbia"),
+        String::from("New Jersey"),
+    ];
+    println!("{}", address_portions.join(", "));
+
+    let address = address_portions
+        .into_iter()
+        .reduce(|mut accumulator, portion| {
+            accumulator.push_str(", ");
+            accumulator.push_str(&portion);
+            accumulator
         });
-    println!("{map:?}");
+    println!("{address:?}");
 }
