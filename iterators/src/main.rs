aaa466ddb981c63126ff0148744c887b2f80e13b
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-20 18:42:24 +0100

    the lines method

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -621,38 +621,52 @@
 
 // --------------------------------------------------------- //
 
-#[derive(Debug)]
-struct GasStation {
-    snack_count: u32,
-    manager: String,
-    employee_count: u32,
+// #[derive(Debug)]
+// struct GasStation {
+//     snack_count: u32,
+//     manager: String,
+//     employee_count: u32,
+// }
+
+// fn main() {
+//     let mobil = GasStation {
+//         snack_count: 100,
+//         manager: String::from("Meg Mobil"),
+//         employee_count: 3,
+//     };
+
+//     let exxon = GasStation {
+//         snack_count: 130,
+//         manager: String::from("Eric Exxon"),
+//         employee_count: 4,
+//     };
+
+//     let shell = GasStation {
+//         snack_count: 50,
+//         manager: String::from("Shane Shell"),
+//         employee_count: 2,
+//     };
+
+//     let mut stops = [mobil, exxon, shell];
+
+//     stops.sort_by_key(|station| station.snack_count);
+//     println!("{stops:#?}");
+
+//     stops.sort_by_key(|station| -(station.employee_count as i32));
+//     println!("{stops:#?}");
+// }
+
+// --------------------------------------------------------- //
+
+use std::fs;
+use std::io;
+
+fn main() -> Result<(), io::Error> {
+    let contents = fs::read_to_string("story.txt")?;
+
+    for line in contents.lines() {
+        println!("{line}");
+    }
+
+    Ok(())
 }
-
-fn main() {
-    let mobil = GasStation {
-        snack_count: 100,
-        manager: String::from("Meg Mobil"),
-        employee_count: 3,
-    };
-
-    let exxon = GasStation {
-        snack_count: 130,
-        manager: String::from("Eric Exxon"),
-        employee_count: 4,
-    };
-
-    let shell = GasStation {
-        snack_count: 50,
-        manager: String::from("Shane Shell"),
-        employee_count: 2,
-    };
-
-    let mut stops = [mobil, exxon, shell];
-
-    stops.sort_by_key(|station| station.snack_count);
-    println!("{stops:#?}");
-
-    stops.sort_by_key(|station| -(station.employee_count as i32));
-    println!("{stops:#?}");
-}
-
