47bebf5e66f44fbde5d96da26b190a8fb8986c21
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-20 16:27:14 +0100

    the sort and sort_by_key methods

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -609,12 +609,50 @@
 
 // --------------------------------------------------------- //
 
-fn main() {
-    let fifty_numbers = 1..=50;
+// fn main() {
+//     let fifty_numbers = 1..=50;
 
-    for number in fifty_numbers.clone().take(15).skip(5).step_by(2) {
-        println!("{number}/");
-    }
+//     for number in fifty_numbers.clone().take(15).skip(5).step_by(2) {
+//         println!("{number}/");
+//     }
 
-    println!("{fifty_numbers:?}");
+//     println!("{fifty_numbers:?}");
+// }
+
+// --------------------------------------------------------- //
+
+#[derive(Debug)]
+struct GasStation {
+    snack_count: u32,
+    manager: String,
+    employee_count: u32,
 }
+
+fn main() {
+    let mobil = GasStation {
+        snack_count: 100,
+        manager: String::from("Meg Mobil"),
+        employee_count: 3,
+    };
+
+    let exxon = GasStation {
+        snack_count: 130,
+        manager: String::from("Eric Exxon"),
+        employee_count: 4,
+    };
+
+    let shell = GasStation {
+        snack_count: 50,
+        manager: String::from("Shane Shell"),
+        employee_count: 2,
+    };
+
+    let mut stops = [mobil, exxon, shell];
+
+    stops.sort_by_key(|station| station.snack_count);
+    println!("{stops:#?}");
+
+    stops.sort_by_key(|station| -(station.employee_count as i32));
+    println!("{stops:#?}");
+}
+
