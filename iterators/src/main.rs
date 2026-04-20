6cd8887dfaf4ba1e194789afecc04e307e93a777
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-20 22:10:16 +0100

    collecting command line arguments I

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -658,15 +658,27 @@
 
 // --------------------------------------------------------- //
 
-use std::fs;
-use std::io;
+// use std::fs;
+// use std::io;
 
-fn main() -> Result<(), io::Error> {
-    let contents = fs::read_to_string("story.txt")?;
+// fn main() -> Result<(), io::Error> {
+//     let contents = fs::read_to_string("story.txt")?;
 
-    for line in contents.lines() {
-        println!("{line}");
+//     for line in contents.lines() {
+//         println!("{line}");
+//     }
+
+//     Ok(())
+// }
+
+// --------------------------------------------------------- //
+
+use std::env;
+
+fn main() {
+    let args = env::args();
+
+    for arg in args {
+        println!("{arg}");
     }
-
-    Ok(())
 }
