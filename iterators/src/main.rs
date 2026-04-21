b8529113c57c396a6c7a43bea1b4fcd3d1167244
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-21 17:16:32 +0100

    form iterator trait

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -723,20 +723,65 @@
 
 // --------------------------------------------------------- //
 
-use std::fs;
-use std::io;
+// use std::fs;
+// use std::io;
 
-fn main() -> io::Result<()> {
-    for entry_result in fs::read_dir("./")? {
-        let entry = entry_result?;
-        let entry_name = entry.path();
-        let metadata = fs::metadata(&entry_name)?;
-        if metadata.is_file() {
-            println!("{entry_name:?}\n-------------");
-            let contents = fs::read_to_string(&entry_name)?;
-            println!("{contents}");
+// fn main() -> io::Result<()> {
+//     for entry_result in fs::read_dir("./")? {
+//         let entry = entry_result?;
+//         let entry_name = entry.path();
+//         let metadata = fs::metadata(&entry_name)?;
+//         if metadata.is_file() {
+//             println!("{entry_name:?}\n-------------");
+//             let contents = fs::read_to_string(&entry_name)?;
+//             println!("{contents}");
+//         }
+//     }
+
+//     Ok(())
+// }
+
+// --------------------------------------------------------- //
+
+use std::collections::HashSet;
+
+#[derive(Debug)]
+struct Playlist {
+    songs: Vec<String>,
+    users: HashSet<String>,
+}
+
+impl FromIterator<(String, String)> for Playlist {
+    fn from_iter<T: IntoIterator<Item = (String, String)>>(iter: T) -> Self {
+        let mut songs = Vec::new();
+        let mut users = HashSet::new();
+        for (song, user) in iter {
+            songs.push(song);
+            users.insert(user);
         }
+        Self { songs, users }
     }
+}
+fn main() {
+    let fifty_numbers = 1..=50;
 
-    Ok(())
+    let results = Vec::from_iter(fifty_numbers.clone());
+    println!("{results:?}");
+
+    let results: Vec<_> = fifty_numbers.clone().collect();
+    println!("{results:?}");
+
+    let unique_set: HashSet<_> = HashSet::from_iter(fifty_numbers);
+    println!("{unique_set:?}");
+
+    let songs = [
+        (String::from("I Rust Go On"), String::from("Bob")),
+        (String::from("A Rust of Wind"), String::from("Bob")),
+        (String::from("A Rustworthy Man"), String::from("Sheila")),
+    ];
+
+    // let playlist: Playlist = Playlist::from_iter(songs);
+    // println!("{playlist:?}");
+
+    let playlist = songs.into_iter().collect::<Playlist>();
 }
