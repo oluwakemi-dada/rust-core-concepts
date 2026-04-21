c67b7e854d885c5ff4b7f8175a530bc960ecad24
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-21 22:28:17 +0100

    the assert! macro

--- testing/src/lib.rs
+++ testing/src/lib.rs
@@ -35,4 +35,21 @@ mod tests {
         museum.sell_ticket();
         assert_eq!(museum.revenue, 25)
     }
+
+    #[test]
+    fn museum_can_sell_multiple_tickets() {
+        let mut museum = Museum::new();
+        museum.sell_ticket();
+        museum.sell_ticket();
+        assert_eq!(museum.revenue, 50);
+    }
+
+    #[test]
+    fn museum_can_have_impressive_art_collection() {
+        let mut museum = Museum::new();
+        museum.buy_painting("Mona Lisa");
+        museum.buy_painting("The Starry Night");
+        museum.buy_painting("Girl with a Pearl Earring");
+        assert!(museum.has_impressive_collection());
+    }
 }
