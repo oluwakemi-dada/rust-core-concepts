b5ff7360b2d09572f9fbbc8d46db4492249b4b79
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-22 19:22:22 +0100

    documentation tests

--- testing/src/attractions.rs
+++ testing/src/attractions.rs
@@ -11,12 +11,35 @@ pub struct Museum {
 impl Museum {
     const MAXIMUM_CAPACITY: usize = 3;
 
+    /// Creates a new Museum instance.
+    ///
+    /// # Examples
+    /// ```
+    /// use testing::attractions::Museum;
+    ///
+    /// let museum = Museum::new();
+    /// let empty_vec: Vec<String> = Vec::new();
+    /// assert_eq!(museum.paintings, empty_vec);
+    /// assert_eq!(museum.revenue, 0);
+    /// ```
+
     pub fn new() -> Self {
         Self {
             paintings: vec![],
             revenue: 0,
         }
     }
+
+    /// Buys a painting for the museum.
+    ///
+    /// # Examples
+    /// ```
+    /// use testing::attractions::Museum;
+    ///
+    /// let mut museum = Museum::new();
+    /// museum.buy_painting("Mona Lisa");
+    /// assert_eq!(museum.paintings, vec!["Mona Lisa".to_string()]);
+    /// ```
 
     pub fn buy_painting(&mut self, painting: &str) {
         if self.paintings.len() >= Self::MAXIMUM_CAPACITY {
@@ -78,12 +101,6 @@ mod tests {
                 "The revenue from selling 1 ticket did not match expectations",
             ))
         }
-
-        // assert_ne!(museum.revenue, 0);
-        // assert_eq!(
-        //     museum.revenue, 25,
-        //     "The revenue from selling 1 ticket did not match expectations"
-        // )
     }
 
     #[test]
@@ -108,11 +125,6 @@ mod tests {
                 "The museum did not have an impressive collection despite having more than 2 paintings",
             ))
         }
-
-        // assert!(
-        //     museum.has_impressive_collection(),
-        //     "The museum did not have an impressive collection despite having more than 2 paintings"
-        // );
     }
 
     #[test]
