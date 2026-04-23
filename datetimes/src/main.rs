7e968ed52fb22c2918c79ec323192f39b32edc97
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-23 16:19:13 +0100

    adding and subtracting time

--- datetimes/src/main.rs
+++ datetimes/src/main.rs
@@ -53,7 +53,7 @@ use chrono::{NaiveDate, TimeDelta};
 use std::ops::{Add, Sub};
 
 fn main() {
-    let birthday = NaiveDate::from_ymd_opt(1991, 4, 12).unwrap();
+    let birthday = NaiveDate::from_ymd_opt(1997, 11, 29).unwrap();
     println!("{}", birthday.add(TimeDelta::days(5)));
 
     println!("{}", birthday.add(TimeDelta::weeks(2) + TimeDelta::days(5)));
