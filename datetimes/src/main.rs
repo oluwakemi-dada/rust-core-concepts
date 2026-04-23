58162deba94495d9d2bce4bf987a197d9e51e81b
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-23 15:13:19 +0100

    the naive date struct

--- datetimes/src/main.rs
+++ datetimes/src/main.rs
@@ -1,4 +1,9 @@
+use chrono::NaiveDate;
 
 fn main() {
-
+    let birthday = "1997-11-29";
+    let birthday = birthday
+        .parse::<NaiveDate>()
+        .expect("Unable to parse NaiveDate from string");
+    println!("{birthday:?}");
 }
