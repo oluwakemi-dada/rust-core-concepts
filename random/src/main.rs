5ab704d01274a19cde945160a560157372a73a7e
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-23 11:32:25 +0100

    intro to the rand crate

--- random/src/main.rs
+++ random/src/main.rs
@@ -1,1 +1,9 @@
-fn main() {}
+use rand::random;
+
+fn main() {
+    let random_float: f64 = random();
+    println!("{}", random_float * 100.0);
+
+    let random_int = random::<u8>();
+    println!("{}", random_int);
+}
