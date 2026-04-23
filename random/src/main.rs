1b7ff6389b92b66804c2f6a1363a591fcc2a35ad
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-23 11:45:36 +0100

    the threadrng struct

--- random/src/main.rs
+++ random/src/main.rs
@@ -1,9 +1,30 @@
-use rand::random;
+// use rand::random;
+
+// fn main() {
+//     let random_float: f64 = random();
+//     println!("{}", random_float * 100.0);
+
+//     let random_int = random::<u8>();
+//     println!("{}", random_int);
+// }
+
+// ---------------------------------------------------- //
+
+use rand::{rng, Rng};
 
 fn main() {
-    let random_float: f64 = random();
-    println!("{}", random_float * 100.0);
+    let mut my_rng = rng();
+    let random_float = my_rng.random::<f64>();
+    println!("{}", random_float);
 
-    let random_int = random::<u8>();
-    println!("{}", random_int);
+    let ten_random_values = (0..10).map(|_| my_rng.random::<i8>()).collect::<Vec<i8>>();
+    println!("{:?}", ten_random_values);
+
+    let random_number: i32 = my_rng.random_range(29..53);
+    println!("{random_number}",);
+
+    println!("{}", my_rng.random_bool(0.9));
+
+    println!("{}", my_rng.random_ratio(1, 2));
+    println!("{}", my_rng.random_ratio(9, 13));
 }
