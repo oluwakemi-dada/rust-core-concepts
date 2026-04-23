3c34cec9072405b7a124f6f1f2ec7a1074c9dce8
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-23 11:50:11 +0100

    randomizing vector elements with shuffle method

--- random/src/main.rs
+++ random/src/main.rs
@@ -10,21 +10,40 @@
 
 // ---------------------------------------------------- //
 
-use rand::{rng, Rng};
+// use rand::{rng, Rng};
+
+// fn main() {
+//   let mut my_rng = rng();
+//   let random_float = my_rng.random::<f64>();
+//   println!("{}", random_float);
+  
+//   let ten_random_values = (0..10).map(|_| my_rng.random::<i8>()).collect::<Vec<i8>>();
+//   println!("{:?}", ten_random_values);
+  
+//   let random_number: i32 = my_rng.random_range(29..53);
+//   println!("{random_number}",);
+  
+//   println!("{}", my_rng.random_bool(0.9));
+  
+//   println!("{}", my_rng.random_ratio(1, 2));
+//   println!("{}", my_rng.random_ratio(9, 13));
+// }
+
+// ---------------------------------------------------- //
+
+use rand::rng;
+use rand::seq::SliceRandom;
 
 fn main() {
     let mut my_rng = rng();
-    let random_float = my_rng.random::<f64>();
-    println!("{}", random_float);
+    let mut candies = vec![
+        "Sour Patch Kids",
+        "Kit Kat",
+        "Twix",
+        "Snickers",
+        "Starburst",
+    ];
+    candies.shuffle(&mut my_rng);
 
-    let ten_random_values = (0..10).map(|_| my_rng.random::<i8>()).collect::<Vec<i8>>();
-    println!("{:?}", ten_random_values);
-
-    let random_number: i32 = my_rng.random_range(29..53);
-    println!("{random_number}",);
-
-    println!("{}", my_rng.random_bool(0.9));
-
-    println!("{}", my_rng.random_ratio(1, 2));
-    println!("{}", my_rng.random_ratio(9, 13));
+    println!("{:?}", candies);
 }
