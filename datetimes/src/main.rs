4b5af0e7070c836c417cb56005c88ada646db6ae
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-23 18:39:25 +0100

    coding challenge

--- datetimes/src/main.rs
+++ datetimes/src/main.rs
@@ -1,3 +1,58 @@
+use chrono::prelude::*;
+
+fn main() {
+    let event_data = vec![
+        (
+            "2025**04**19 !! 16:00:00 -04:00",
+            "Started Rust study session",
+        ),
+        ("2025**04**20 !! 08:05:30 -04:00", "Made breakfast"),
+        ("ERR", "ERR"),
+        ("2025**04**22 !! 22:10:45 -04:00", "Went to bed"),
+        ("ERR", "ERR"),
+        ("2025**04**25 !! 09:00:03 -04:00", "Resumed Rust study"),
+    ];
+
+    let format = "%Y**%m**%d !! %H:%M:%S %z";
+
+    let events: Vec<_> = event_data
+        .into_iter()
+        .filter_map(|(timestamp, message)| {
+            let parsed_datetime = DateTime::parse_from_str(timestamp, format);
+            match parsed_datetime {
+                Ok(datetime) => Some((datetime.with_timezone(&Utc), message)),
+                Err(_) => None,
+            }
+        })
+        .collect();
+
+    let mut previous_event: Option<DateTime<Utc>> = None;
+
+    for (utc_datetime, message) in events {
+        let display_time = utc_datetime.format("%Y-%m-%d %H:%M:%S");
+        println!("Event time: {display_time}");
+        println!("Event message: {message}");
+
+        if let Some(previous_datetime) = previous_event {
+            let difference = utc_datetime - previous_datetime;
+            let hours = difference.num_hours();
+            let minutes = difference.num_minutes() % 60;
+            let seconds = difference.num_seconds() % 60;
+            println!(
+                "Time since previous event: {}h {}m {}s",
+                hours, minutes, seconds
+            );
+        }
+
+        println!();
+        previous_event = Some(utc_datetime);
+    }
+}
+
+
+// ------------------------------------------------------------------- //
+
+
 // use chrono::NaiveDate;
 
 // fn main() {
@@ -165,15 +220,15 @@
 
 // ------------------------------------------------------------------- //
 
-use chrono::prelude::*;
+// use chrono::prelude::*;
 
-fn main() {
-    let utc_time = Utc::now();
-    println!("{}", utc_time.format("%m-%d-%Y"));
-    println!("{}", utc_time.format("%m/%d/%Y"));
-    println!("{}", utc_time.format("%m/%d/%y"));
+// fn main() {
+//     let utc_time = Utc::now();
+//     println!("{}", utc_time.format("%m-%d-%Y"));
+//     println!("{}", utc_time.format("%m/%d/%Y"));
+//     println!("{}", utc_time.format("%m/%d/%y"));
 
-    println!("{}", utc_time.format("%b %d, %y %H:%M:%S"));
+//     println!("{}", utc_time.format("%b %d, %y %H:%M:%S"));
 
-    println!("{}", utc_time.format("%A %I:%M %p %Z"));
-}
+//     println!("{}", utc_time.format("%A %I:%M %p %Z"));
+// }
