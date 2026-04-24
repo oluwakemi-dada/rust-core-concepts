19671761d3f4735d4923e5892206c61e02ba8013
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-24 18:24:04 +0100

    box vs regular references I

--- smart-pointers-box/src/main.rs
+++ smart-pointers-box/src/main.rs
@@ -28,37 +28,89 @@
 // }
 
 // ---------------------------------------------------------- //
+
+// #[derive(Debug)]
+// enum LinkedList<T> {
+//     Empty,
+//     Node { value: T, next: Box<LinkedList<T>> },
+// }
+
+// fn main() {
+//     let list = LinkedList::Node {
+//         value: 1.3,
+//         next: Box::new(LinkedList::Node {
+//             value: 2.6,
+//             next: Box::new(LinkedList::Node {
+//                 value: 3.9,
+//                 next: Box::new(LinkedList::Empty),
+//             }),
+//         }),
+//     };
+//     println!("{:#?}", list);
+
+//     let im_with_you = LinkedList::Node {
+//         value: String::from("I'm With You"),
+//         next: Box::new(LinkedList::Empty),
+//     };
+//     let sk8er_boi = LinkedList::Node {
+//         value: String::from("Sk8er Boi"),
+//         next: Box::new(im_with_you),
+//     };
+//     let complicated = LinkedList::Node {
+//         value: String::from("Complicated"),
+//         next: Box::new(sk8er_boi),
+//     };
+
+//     println!("{complicated:#?}");
+// }
+
+// ---------------------------------------------------------- //
+
 #[derive(Debug)]
-enum LinkedList<T> {
+enum LinkedListUsingBox<T> {
     Empty,
-    Node { value: T, next: Box<LinkedList<T>> },
+    Node {
+        value: T,
+        next: Box<LinkedListUsingBox<T>>,
+    },
+}
+
+#[derive(Debug)]
+enum LinkedListUsingReference<'a, T> {
+    Empty,
+    Node {
+        value: T,
+        next: &'a LinkedListUsingReference<'a, T>,
+    },
 }
 
 fn main() {
-    let list = LinkedList::Node {
-        value: 1.3,
-        next: Box::new(LinkedList::Node {
-            value: 2.6,
-            next: Box::new(LinkedList::Node {
-                value: 3.9,
-                next: Box::new(LinkedList::Empty),
-            }),
-        }),
-    };
-    println!("{:#?}", list);
-
-    let im_with_you = LinkedList::Node {
-        value: String::from("I'm With You"),
-        next: Box::new(LinkedList::Empty),
-    };
-    let sk8er_boi = LinkedList::Node {
-        value: String::from("Sk8er Boi"),
-        next: Box::new(im_with_you),
-    };
-    let complicated = LinkedList::Node {
-        value: String::from("Complicated"),
-        next: Box::new(sk8er_boi),
+    let second_node = LinkedListUsingReference::Node {
+        value: 2,
+        next: &LinkedListUsingReference::Empty,
     };
 
-    println!("{complicated:#?}");
+    let first_node = LinkedListUsingReference::Node {
+        value: 1,
+        next: &second_node,
+    };
+
+    println!("{first_node:#?}");
+    println!("{second_node:#?}");
+
+
+    let second_node = LinkedListUsingBox::Node {
+        value: 2,
+        next: Box::new(LinkedListUsingBox::Empty),
+    };
+
+    let first_node = LinkedListUsingBox::Node {
+        value: 1,
+        next: Box::new(second_node),
+    };
+
+    // drop(first_node);
+    println!("{first_node:#?}");
+    // println!("{second_node:#?}");
+
 }
