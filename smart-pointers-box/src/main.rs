7c49f4cd0ba9760f820e4dd14e5969983c35bd88
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-24 11:09:29 +0100

    smart pointers

--- smart-pointers-box/src/main.rs
+++ smart-pointers-box/src/main.rs
@@ -1,1 +1,14 @@
-fn main() {}
+fn main() {
+    let mut sushi = String::from("Yellowtail");
+    let sushi_raw_pointer_1 = &raw const sushi;
+    let sushi_raw_pointer_2: *const String = &sushi;
+    let sushi_raw_mutable_pointer_1 = &raw mut sushi;
+    let sushi_raw_mutable_pointer_2 = &raw mut sushi;
+
+    drop(sushi);
+
+    unsafe {
+        println!("{}", *sushi_raw_pointer_1);
+    }
+    println!("4")
+}
