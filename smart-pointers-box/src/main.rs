14c270f6089fe396426720b85b04f9ebcdea29c3
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-24 11:46:21 +0100

    the box smart pointer

--- smart-pointers-box/src/main.rs
+++ smart-pointers-box/src/main.rs
@@ -1,14 +1,27 @@
+// fn main() {
+//     let mut sushi = String::from("Yellowtail");
+//     let sushi_raw_pointer_1 = &raw const sushi;
+//     let sushi_raw_pointer_2: *const String = &sushi;
+//     let sushi_raw_mutable_pointer_1 = &raw mut sushi;
+//     let sushi_raw_mutable_pointer_2 = &raw mut sushi;
+
+//     drop(sushi);
+
+//     unsafe {
+//         println!("{}", *sushi_raw_pointer_1);
+//     }
+//     println!("4")
+// }
+
+// ---------------------------------------------------------- //
 fn main() {
-    let mut sushi = String::from("Yellowtail");
-    let sushi_raw_pointer_1 = &raw const sushi;
-    let sushi_raw_pointer_2: *const String = &sushi;
-    let sushi_raw_mutable_pointer_1 = &raw mut sushi;
-    let sushi_raw_mutable_pointer_2 = &raw mut sushi;
+    let my_box = Box::new(100);
 
-    drop(sushi);
+    println!("{}", *my_box);
+    println!("{my_box}");
+    println!("{}", my_box);
+    println!("{:?}", *my_box);
 
-    unsafe {
-        println!("{}", *sushi_raw_pointer_1);
-    }
-    println!("4")
+    let your_box = my_box;
+    println!("{your_box}");
 }
