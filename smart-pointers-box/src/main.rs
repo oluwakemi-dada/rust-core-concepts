bbc0976db6949e8ab19e488ce61a95fa863adbdb
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-27 15:33:13 +0100

    trait objects I

--- smart-pointers-box/src/main.rs
+++ smart-pointers-box/src/main.rs
@@ -284,13 +284,62 @@
 
 // ---------------------------------------------------------- //
 
-fn main() {
-    let text = String::from("Hello");
-    let my_box = Box::new(text);
-    let value = &(*my_box)[..];
-    output_text(value);
+// fn main() {
+//     let text = String::from("Hello");
+//     let my_box = Box::new(text);
+//     let value = &(*my_box)[..];
+//     output_text(value);
+// }
+
+// fn output_text(text: &str) {
+//     println!("{}", text);
+// }
+
+// ---------------------------------------------------------- //
+
+trait Wearable {
+    fn wear(&self) -> String;
 }
 
-fn output_text(text: &str) {
-    println!("{}", text);
+#[derive(Debug)]
+struct Pants {
+    fabric: String,
+    waist_size: u32,
+}
+
+impl Wearable for Pants {
+    fn wear(&self) -> String {
+        format!("{} {} pants", self.waist_size, self.fabric)
+    }
+}
+
+#[derive(Debug)]
+struct Tie {
+    color: String,
+}
+
+impl Wearable for Tie {
+    fn wear(&self) -> String {
+        format!("{} tie", self.color)
+    }
+}
+
+fn main() {
+    let pants = Pants {
+        fabric: "Cotton".to_string(),
+        waist_size: 34,
+    };
+
+    let tie = Tie {
+        color: "Red".to_string(),
+    };
+
+    let outfit: Vec<Box<dyn Wearable>> = vec![Box::new(pants), Box::new(tie)];
+
+    // for item in outfit {
+    //     println!("Putting on the {}", item.wear());
+    // }
+
+    let items: Vec<String> = outfit.iter().map(|item| item.wear()).collect();
+    println!("{:?}", items); // ["Cotton pants", "Red tie"]
 }
