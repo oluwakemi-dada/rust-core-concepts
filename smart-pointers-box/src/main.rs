7af288410ebd296e51dcdabe1955c2474acd5c4f
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-27 17:33:39 +0100

    custom error types

--- smart-pointers-box/src/main.rs
+++ smart-pointers-box/src/main.rs
@@ -347,17 +347,35 @@
 // ---------------------------------------------------------- //
 
 use std::error::Error;
+use std::fmt::Display;
 use std::fs;
 
+#[derive(Debug)]
+struct NumberIsUnimpressiveError;
+
+impl Display for NumberIsUnimpressiveError {
+    fn fmt(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(formatter, "That number is just too small")
+    }
+}
+
+impl Error for NumberIsUnimpressiveError {}
+
 fn read_number_from_file(path: &str) -> Result<i32, Box<dyn Error>> {
-    let file_contents = match fs::read_to_string(path) {
+    let file_read = match fs::read_to_string(path) {
         Ok(content) => content,
         Err(error) => return Err(Box::new(error)),
     };
 
-    match file_contents.parse::<i32>() {
-        Ok(number) => Ok(number),
-        Err(error) => Err(Box::new(error)),
+    let number = match file_read.trim().parse::<i32>() {
+        Ok(number) => number,
+        Err(error) => return Err(Box::new(error)),
+    };
+
+    if number < 100 {
+        Err(Box::new(NumberIsUnimpressiveError))
+    } else {
+        Ok(number)
     }
 }
 
