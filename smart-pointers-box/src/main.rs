47ad40931ac40ce6e5556cce2919f1d7e539c094
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-27 12:32:45 +0100

    create a binary search tree

--- smart-pointers-box/src/main.rs
+++ smart-pointers-box/src/main.rs
@@ -105,35 +105,97 @@
 
 // ---------------------------------------------------------- //
 
+// #[derive(Debug)]
+// enum FileSystemEntity {
+//     File {
+//         name: String,
+//     },
+//     Folder {
+//         name: String,
+//         content: Vec<FileSystemEntity>,
+//     },
+// }
+
+// fn main() {
+//     let rust_file = FileSystemEntity::File {
+//         name: String::from("my_rust_code.rs"),
+//     };
+//     let python_file = FileSystemEntity::File {
+//         name: String::from("my_python_code.py"),
+//     };
+//         let code_folder = FileSystemEntity::Folder {
+//         name: String::from("Code Stuff"),
+//         content: vec![rust_file, python_file],
+//     };
+//     let screenplay = FileSystemEntity::File {
+//         name: String::from("My Screenplay.txt"),
+//     };
+//     let all_documents = FileSystemEntity::Folder {
+//         name: String::from("Documents"),
+//         content: vec![screenplay, code_folder],
+//     };
+
+//     println!("{all_documents:#?}");
+// }
+
+// ---------------------------------------------------------- //
+
+use std::cmp::Ordering;
+
 #[derive(Debug)]
-enum FileSystemEntity {
-    File {
-        name: String,
-    },
-    Folder {
-        name: String,
-        content: Vec<FileSystemEntity>,
+enum BinarySearchTree {
+    Empty,
+    Node {
+        value: i32,
+        left: Box<BinarySearchTree>,
+        right: Box<BinarySearchTree>,
     },
 }
 
-fn main() {
-    let rust_file = FileSystemEntity::File {
-        name: String::from("my_rust_code.rs"),
-    };
-    let python_file = FileSystemEntity::File {
-        name: String::from("my_python_code.py"),
-    };
-        let code_folder = FileSystemEntity::Folder {
-        name: String::from("Code Stuff"),
-        content: vec![rust_file, python_file],
-    };
-    let screenplay = FileSystemEntity::File {
-        name: String::from("My Screenplay.txt"),
-    };
-    let all_documents = FileSystemEntity::Folder {
-        name: String::from("Documents"),
-        content: vec![screenplay, code_folder],
-    };
+impl BinarySearchTree {
+    fn new() -> Self {
+        BinarySearchTree::Empty
+    }
 
-    println!("{all_documents:#?}");
+    fn insert(&mut self, new_value: i32) {
+        match self {
+            BinarySearchTree::Empty => {
+                *self = BinarySearchTree::Node {
+                    value: new_value,
+                    left: Box::new(BinarySearchTree::Empty),
+                    right: Box::new(BinarySearchTree::Empty),
+                }
+            }
+            BinarySearchTree::Node { value, left, right } => match new_value.cmp(value) {
+                Ordering::Equal => (),
+                Ordering::Less => left.insert(new_value),
+                Ordering::Greater => right.insert(new_value),
+            },
+        }
+    }
+
+    fn contains(&self, target: i32) -> bool {
+        match self {
+            BinarySearchTree::Empty => false,
+            BinarySearchTree::Node { value, left, right } => match target.cmp(value) {
+                Ordering::Equal => true,
+                Ordering::Less => left.contains(target),
+                Ordering::Greater => right.contains(target),
+            },
+        }
+    }
+}
+
+fn main() {
+    let mut tree = BinarySearchTree::new(); // []
+    tree.insert(5);
+    tree.insert(2);
+    tree.insert(8);
+    tree.insert(4);
+    tree.insert(13);
+    println!("{tree:#?}");
+
+    println!("{}", tree.contains(13)); // true
+    println!("{}", tree.contains(4)); // true
+    println!("{}", tree.contains(21)); // false
 }
