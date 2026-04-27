997c39ae8bc534d64df631999fa05fad37b3171a
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-27 13:35:35 +0100

    the deref and derefmut trait II

--- smart-pointers-box/src/main.rs
+++ smart-pointers-box/src/main.rs
@@ -202,38 +202,72 @@
 
 // ---------------------------------------------------------- //
 
+// use std::ops::{Deref, DerefMut};
+
+// struct CustomBox<T> {
+//     data: T,
+// }
+
+// impl<T> CustomBox<T> {
+//     fn new(data: T) -> Self {
+//         Self { data }
+//     }
+// }
+
+// impl<T> Deref for CustomBox<T> {
+//     type Target = T;
+
+//     fn deref(&self) -> &Self::Target {
+//         &self.data
+//     }
+// }
+
+// impl<T> DerefMut for CustomBox<T> {
+//     fn deref_mut(&mut self) -> &mut Self::Target {
+//         &mut self.data
+//     }
+// }
+
+// fn main() {
+//     let mut boxy: Box<f64> = Box::new(3.14);
+//     *boxy = 6.28;
+//     println!("{}", *boxy); // 25
+
+//     let mut custom_boxy = CustomBox::new(4);
+//     *custom_boxy = 25;
+//     println!("{}", *custom_boxy);
+// }
+
+// ---------------------------------------------------------- //
+
 use std::ops::{Deref, DerefMut};
 
-struct CustomBox<T> {
+struct CustomBox<T, U> {
     data: T,
+    more_data: U,
 }
 
-impl<T> CustomBox<T> {
-    fn new(data: T) -> Self {
-        Self { data }
+impl<T, U> CustomBox<T, U> {
+    fn new(data: T, more_data: U) -> Self {
+        Self { data, more_data }
     }
 }
 
-impl<T> Deref for CustomBox<T> {
-    type Target = T;
+impl<T, U> Deref for CustomBox<T, U> {
+    type Target = U;
 
     fn deref(&self) -> &Self::Target {
-        &self.data
+        &self.more_data
     }
 }
 
-impl<T> DerefMut for CustomBox<T> {
+impl<T, U> DerefMut for CustomBox<T, U> {
     fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.data
+        &mut self.more_data
     }
 }
 
 fn main() {
-    let mut boxy: Box<f64> = Box::new(3.14);
-    *boxy = 6.28;
-    println!("{}", *boxy); // 25
-
-    let mut custom_boxy = CustomBox::new(4);
-    *custom_boxy = 25;
-    println!("{}", *custom_boxy);
+    let boxy = CustomBox::new("Ham and Eggs", 15);
+    println!("{}", *boxy); // 15
 }
