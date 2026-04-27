e00561d3cc6946778df0f0bb33021e183a139de3
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-27 14:30:32 +0100

    the deref coersion

--- smart-pointers-box/src/main.rs
+++ smart-pointers-box/src/main.rs
@@ -240,44 +240,57 @@
 
 // ---------------------------------------------------------- //
 
-use std::ops::{Deref, DerefMut};
+// use std::ops::{Deref, DerefMut};
 
-struct CustomBox<T, U> {
-    data: T,
-    more_data: U,
-}
+// struct CustomBox<T, U> {
+//     data: T,
+//     more_data: U,
+// }
 
-impl<T, U> CustomBox<T, U> {
-    fn new(data: T, more_data: U) -> Self {
-        Self { data, more_data }
-    }
-}
+// impl<T, U> CustomBox<T, U> {
+//     fn new(data: T, more_data: U) -> Self {
+//         Self { data, more_data }
+//     }
+// }
 
-impl<T, U> Deref for CustomBox<T, U> {
-    type Target = U;
+// impl<T, U> Deref for CustomBox<T, U> {
+//     type Target = U;
 
-    fn deref(&self) -> &Self::Target {
-        &self.more_data
-    }
-}
+//     fn deref(&self) -> &Self::Target {
+//         &self.more_data
+//     }
+// }
 
-impl<T, U> DerefMut for CustomBox<T, U> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.more_data
-    }
-}
-impl<T, U> Drop for CustomBox<T, U> {
-    fn drop(&mut self) {
-        println!("I'm removing the CustomBox from memory!");
-    }
-}
+// impl<T, U> DerefMut for CustomBox<T, U> {
+//     fn deref_mut(&mut self) -> &mut Self::Target {
+//         &mut self.more_data
+//     }
+// }
+// impl<T, U> Drop for CustomBox<T, U> {
+//     fn drop(&mut self) {
+//         println!("I'm removing the CustomBox from memory!");
+//     }
+// }
+
+// fn main() {
+//     let mut boxy: Box<f64> = Box::new(3.14);
+//     *boxy = 6.28;
+//     println!("{}", *boxy); // 25
+
+//     let mut custom_boxy = CustomBox::new(3.14, "Hello");
+//     *custom_boxy = "Goodbye";
+//     println!("{}", *custom_boxy);
+// }
+
+// ---------------------------------------------------------- //
 
 fn main() {
-    let mut boxy: Box<f64> = Box::new(3.14);
-    *boxy = 6.28;
-    println!("{}", *boxy); // 25
+    let text = String::from("Hello");
+    let my_box = Box::new(text);
+    let value = &(*my_box)[..];
+    output_text(value);
+}
 
-    let mut custom_boxy = CustomBox::new(3.14, "Hello");
-    *custom_boxy = "Goodbye";
-    println!("{}", *custom_boxy);
+fn output_text(text: &str) {
+    println!("{}", text);
 }
