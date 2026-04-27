0be0b6b3013c816109ba5366476b89245b5b5f8c
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-27 13:28:22 +0100

    the deref and derefmut trait I

--- smart-pointers-box/src/main.rs
+++ smart-pointers-box/src/main.rs
@@ -140,62 +140,100 @@
 
 // ---------------------------------------------------------- //
 
-use std::cmp::Ordering;
+// use std::cmp::Ordering;
 
-#[derive(Debug)]
-enum BinarySearchTree {
-    Empty,
-    Node {
-        value: i32,
-        left: Box<BinarySearchTree>,
-        right: Box<BinarySearchTree>,
-    },
+// #[derive(Debug)]
+// enum BinarySearchTree {
+//     Empty,
+//     Node {
+//         value: i32,
+//         left: Box<BinarySearchTree>,
+//         right: Box<BinarySearchTree>,
+//     },
+// }
+
+// impl BinarySearchTree {
+//     fn new() -> Self {
+//         BinarySearchTree::Empty
+//     }
+
+//     fn insert(&mut self, new_value: i32) {
+//         match self {
+//             BinarySearchTree::Empty => {
+//                 *self = BinarySearchTree::Node {
+//                     value: new_value,
+//                     left: Box::new(BinarySearchTree::Empty),
+//                     right: Box::new(BinarySearchTree::Empty),
+//                 }
+//             }
+//             BinarySearchTree::Node { value, left, right } => match new_value.cmp(value) {
+//                 Ordering::Equal => (),
+//                 Ordering::Less => left.insert(new_value),
+//                 Ordering::Greater => right.insert(new_value),
+//             },
+//         }
+//     }
+
+//     fn contains(&self, target: i32) -> bool {
+//         match self {
+//             BinarySearchTree::Empty => false,
+//             BinarySearchTree::Node { value, left, right } => match target.cmp(value) {
+//                 Ordering::Equal => true,
+//                 Ordering::Less => left.contains(target),
+//                 Ordering::Greater => right.contains(target),
+//             },
+//         }
+//     }
+// }
+
+// fn main() {
+//     let mut tree = BinarySearchTree::new(); // []
+//     tree.insert(5);
+//     tree.insert(2);
+//     tree.insert(8);
+//     tree.insert(4);
+//     tree.insert(13);
+//     println!("{tree:#?}");
+
+//     println!("{}", tree.contains(13)); // true
+//     println!("{}", tree.contains(4)); // true
+//     println!("{}", tree.contains(21)); // false
+// }
+
+// ---------------------------------------------------------- //
+
+use std::ops::{Deref, DerefMut};
+
+struct CustomBox<T> {
+    data: T,
 }
 
-impl BinarySearchTree {
-    fn new() -> Self {
-        BinarySearchTree::Empty
+impl<T> CustomBox<T> {
+    fn new(data: T) -> Self {
+        Self { data }
     }
+}
 
-    fn insert(&mut self, new_value: i32) {
-        match self {
-            BinarySearchTree::Empty => {
-                *self = BinarySearchTree::Node {
-                    value: new_value,
-                    left: Box::new(BinarySearchTree::Empty),
-                    right: Box::new(BinarySearchTree::Empty),
-                }
-            }
-            BinarySearchTree::Node { value, left, right } => match new_value.cmp(value) {
-                Ordering::Equal => (),
-                Ordering::Less => left.insert(new_value),
-                Ordering::Greater => right.insert(new_value),
-            },
-        }
+impl<T> Deref for CustomBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.data
     }
+}
 
-    fn contains(&self, target: i32) -> bool {
-        match self {
-            BinarySearchTree::Empty => false,
-            BinarySearchTree::Node { value, left, right } => match target.cmp(value) {
-                Ordering::Equal => true,
-                Ordering::Less => left.contains(target),
-                Ordering::Greater => right.contains(target),
-            },
-        }
+impl<T> DerefMut for CustomBox<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.data
     }
 }
 
 fn main() {
-    let mut tree = BinarySearchTree::new(); // []
-    tree.insert(5);
-    tree.insert(2);
-    tree.insert(8);
-    tree.insert(4);
-    tree.insert(13);
-    println!("{tree:#?}");
+    let mut boxy: Box<f64> = Box::new(3.14);
+    *boxy = 6.28;
+    println!("{}", *boxy); // 25
 
-    println!("{}", tree.contains(13)); // true
-    println!("{}", tree.contains(4)); // true
-    println!("{}", tree.contains(21)); // false
+    let mut custom_boxy = CustomBox::new(4);
+    *custom_boxy = 25;
+    println!("{}", *custom_boxy);
 }
