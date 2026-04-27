15995c885afa8bbd16b9cd6288ac03e2a49bf30e
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-27 22:28:57 +0100

    the refcell type

--- smart-pointers-ii/src/main.rs
+++ smart-pointers-ii/src/main.rs
@@ -1,10 +1,43 @@
-use std::cell::Cell;
+// use std::cell::Cell;
+
+// #[derive(Debug)]
+// struct ConcertTicket {
+//     section: String,
+//     seat: String,
+//     scanned: Cell<bool>,
+// }
+
+// impl ConcertTicket {
+//     fn new(section: String, seat: String) -> Self {
+//         Self {
+//             section,
+//             seat,
+//             scanned: Cell::new(false),
+//         }
+//     }
+
+//     fn admit_attendee(&self) {
+//         self.scanned.set(true);
+//     }
+// }
+
+// fn main() {
+//     let ticket = ConcertTicket::new(String::from("A"), String::from("3"));
+//     println!("{}", ticket.scanned.get());
+
+//     ticket.admit_attendee();
+//     println!("{}", ticket.scanned.get());
+// }
+
+// ---------------------------------------------------- //
+
+use std::cell::RefCell;
 
 #[derive(Debug)]
 struct ConcertTicket {
     section: String,
     seat: String,
-    scanned: Cell<bool>,
+    scanned: bool,
 }
 
 impl ConcertTicket {
@@ -12,19 +45,24 @@ impl ConcertTicket {
         Self {
             section,
             seat,
-            scanned: Cell::new(false),
+            scanned: false,
         }
-    }
-
-    fn admit_attendee(&self) {
-        self.scanned.set(true);
     }
 }
 
 fn main() {
-    let ticket = ConcertTicket::new(String::from("A"), String::from("3"));
-    println!("{}", ticket.scanned.get());
+    let ticket = RefCell::new(ConcertTicket::new(String::from("A"), String::from("3")));
 
-    ticket.admit_attendee();
-    println!("{}", ticket.scanned.get());
+    {
+        let mut one = ticket.borrow_mut();
+        one.seat = String::from("D");
+    }
+
+    // ticket.borrow_mut().seat = String::from("D");
+    println!("{:#?}", ticket.borrow().seat);
+
+    ticket.borrow_mut().seat = String::from("K");
+    println!("{:#?}", ticket.borrow().seat);
+
+    // println!("{:#?}", ticket);
 }
